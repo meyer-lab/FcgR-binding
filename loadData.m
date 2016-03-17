0959202926e47b09a55fcea22edf162a52893a22
Aaron Meyer <a@ameyer.me>
2016-03-17 08:43:51 -0400

    Revert "Times they are a"
    This reverts commit 79e306d464968918192bce79d4e9f41c9032efb3, reversing
    changes made to 8ea8925879df30bdf11f5eb8c84de236dff5d073.

--- loadData.m
+++ loadData.m
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 function [kd, mfiAdjMean4, mfiAdjMean26, kdBruhns] = loadData ()
-=======
-function [mfiAdjMean, kd, tnpbsa, boundExp] = loadData ()
->>>>>>> 15975d1ad7ef19e4b174dacac2ab1a51e6548b92
     mfi = csvread('Luxetal2013-Fig2B.csv',2,2);
     mfi(29,1) = nan;
 
@@ -24,7 +20,6 @@ function [mfiAdjMean, kd, tnpbsa, boundExp] = loadData ()
     mfiAdjMean = zeros(24,8);
     for j = 1:8
         mfiAdjMean(:,j) = mfiAdj(:,j) / nanmean(mfiAdj(:,j));
-<<<<<<< HEAD
     end    
     
     %Separate the MFIs from TNP-4-BSA trials from those from TNP-26-BSA
@@ -35,28 +30,4 @@ function [mfiAdjMean, kd, tnpbsa, boundExp] = loadData ()
     %Load the Kd values found exclusively from Bruhns et al. (2009)
     kdBruhns = csvread('FcR-Kd-2.csv');
     
-=======
-    end
-    
-    %Create a figure for the molarity of TNP-X-BSA in the solution into which 
-    %the CHO cells were placed (see Lux et al. 2013, Figure 2). The molecular
-    %weight of bovine serum albumin is 66463 Da, and the contration of
-    %TNP-X-BSA in the solution described in Figure 2 is 5 micrograms per
-    %milliliter
-    tnpbsa = 1/66463 * 1e-6 * 5 * 1e3;
-
-    %Using a highly simplified model of receptor binding of the form
-    %                   
-    %                     C = R*L_0/(L_0 + K_D),
-    %
-    %granted the Kd values we have compiled, the expected number of bound
-    %immune complexes per receptor for each flavor of receptor for each flavor 
-    %of immunoglobulin should be as follows:
-    boundExp = zeros(6,4);
-    for j = 1:6
-        for k = 1:4
-            boundExp(j,k) = tnpbsa / (tnpbsa + kd(j,k));
-        end
-    end
->>>>>>> 15975d1ad7ef19e4b174dacac2ab1a51e6548b92
 end
