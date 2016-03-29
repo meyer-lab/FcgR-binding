cc53e838b1228695e75939ee5559ddc4a9562df8
rrobinet <rrobinet@mit.edu>
2016-03-29 00:17:18 -0400

    I calculate the binomial coefficients once, and these values are stored in a 10X10 matrix. A matrix of coefficients of the form (v!/(v-i)!*i!)*10^(kx+i-1)*tnpbsa is created twnety times, and this number of calculations cannot be reduced.

--- Bound.m
+++ Bound.m
@@ -1,16 +1,21 @@
-function [ C ] = Bound( r, kx, kd_spec, tnpbsa, v )
-    %Granted a receptor expression level r, a constant kx, the specific Kd
+function [ C ] = Bound( r, kd_spec, tnpbsa, v, CoefMat )
+    %Granted a receptor expression level r, the specific Kd
     %value of the pertinent receptor-immunoglobulin combination kd_spec,
-    %the molarity of TNP-X-BSA used by Lux et al. (see Figure 2), and
-    %avidity v, the number of immune complexes bound to a cell should be
-    %as follows, according to the model from Stone et al.:
+    %the molarity of TNP-X-BSA used by Lux et al. (see Figure 2), valency v,
+    %and a coefficient of the form:
+    %v!/((v-i)!*i!)*10^(kx+i-1)*tnpbsa (many such coefficients being
+    %contained in the matrix CoefMat), the number of immune complexes bound
+    %to a cell should be as follows, according to the model from Stone et al.:
     %
     %   The sum from 1 to v of C_i = v!/((v-i)!*i!)*10^(kx*(i-1))*(tnpbsa/kd_spec)*r^i
     
-    % j = 1
-    C = v*(tnpbsa/kd_spec)*r;
-    
-    for j = 2:v
-        C = C + nchoosek(v,j)*10^(kx*(j-1))*(tnpbsa/kd_spec)*r^j;
+    rKdVec = zeros(1,10);
+    for j = 1:10
+        rKdVec(j) = r^j;
     end
+    rKdVec = rKdVec/kd_spec;
+    
+    CoefVec = CoefMat(:,v);
+
+    C = rKdVec*CoefVec;
 end
