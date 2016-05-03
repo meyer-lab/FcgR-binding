f65423422fa0cd7757bd23d60449b1baa6c653ba
rrobinet <rrobinet@mit.edu>
2016-05-03 07:28:08 -0400

    Trying to get fits with fixed TNP-X-BSA concentrations which match both the TNP-4-BSA trials and TNP-26-BSA trials.

--- Error.m
+++ Error.m
@@ -1,19 +1,22 @@
-function [J, mfiExp] = Error( Rtot, kd, mfiAdjMean, v, biCoefMat)
+function [J, mfiExp] = Error( Rtot, kd, mfiAdjMean4, mfiAdjMean26, v, biCoefMat,tnpbsa)
     Rtot = 10.^Rtot;
     kx = Rtot(7);
-    L = Rtot(8);
+    L = tnpbsa;
     
-    Req = zeros(6,4);
+    Req4 = zeros(6,4);
+    Req26 = zeros(6,4);
     
-    ReqFunc = @(Reqi, R, kdi) R - Reqi*(1+v*L/kdi*(1+kx*Reqi)^(v-1));
+    ReqFunc = @(Reqi, R, kdi, Li) R - Reqi*(1+v*Li/kdi*(1+kx*Reqi)^(v-1));
     
     for j = 1:6
         for k = 1:4
-            Req(j,k) = fzero(@(x) ReqFunc(10^x,Rtot(j),kd(j,k)), -1);
+            Req4(j,k) = fzero(@(x) ReqFunc(10^x,Rtot(j),kd(j,k),L(1)), -1);
+            Req26(j,k) = fzero(@(x) ReqFunc(10^x,Rtot(j),kd(j,k),L(2)), -1);
         end
     end
     
-    Req = 10.^Req;
+    Req4 = 10.^Req4;
+    Req26 = 10.^Req26;
     
 %   R is a seven-dimensional vector whose first six elements are
 %   expression levels of FcgRIA, FcgRIIA-Arg, etc. and whose seventh
@@ -30,20 +33,26 @@ function [J, mfiExp] = Error( Rtot, kd, mfiAdjMean, v, biCoefMat)
     %which the model outputs a minimum error
     
     CoefVec = biCoefMat(1:v,v) .* Rtot(size(Rtot,1)-1).^((1:v)'-1)*Rtot(size(Rtot,1));
-    mfiExpPre = zeros(6,4);
+    mfiExpPre4 = zeros(6,4);
+    mfiExpPre26 = zeros(6,4);
     for j = 1:6
         for k = 1:4
-            mfiExpPre(j,k) = nansum(CoefVec.*Req(j,k).^(1:v)');
+            mfiExpPre4(j,k) = nansum(CoefVec.*Req4(j,k).^(1:v)');
+            mfiExpPre26(j,k) = nansum(CoefVec.*Req26(j,k).^(1:v)');
         end
     end
-    mfiExpPre = mfiExpPre./kd;
+    mfiExpPre4 = mfiExpPre4./kd;
+    mfiExpPre26 = mfiExpPre26./kd;
         
-    mfiExp = zeros(24,4);
+    mfiExp4 = zeros(24,4);
+    mfiExp26 = zeros(24,4);
     for j = 1:6
         for k = 1:4
-            mfiExp((4*(j-1)+k),:) = mfiExpPre(j,k)*ones(1,4);
+            mfiExp4((4*(j-1)+k),:) = mfiExpPre4(j,k)*ones(1,4);
+            mfiExp26((4*(j-1)+k),:) = mfiExpPre26(j,k)*ones(1,4);
         end
     end
+    mfiExp = [mfiExp4, mfiExp26];
     
-    J = nansum(nansum((mfiExp - mfiAdjMean).^2));
+    J = nansum(nansum([(mfiExp4 - mfiAdjMean4).^2, (mfiExp26 - mfiAdjMean26).^2]));
 end
