63a89190d66a12726d9df56b9b3552263bbd2a5e
rrobinet <rrobinet@mit.edu>
2016-05-04 14:00:51 -0400

    Fits the model from Stone et al. using nu = 4 and nu = 26 for all the data in a single reun of the function

--- RegressionAndResiduals.m
+++ RegressionAndResiduals.m
@@ -18,28 +18,12 @@ for j = 1:26
     end
 end
 
-%Set up parameters for GlobalSearch and fmincon
-
-Rc = zeros(7, 10);
-RcFit = zeros(1,size(Rc,2));
-
 opts = optimoptions(@fmincon,'Algorithm','interior-point','Display','off');
 gs = GlobalSearch('StartPointsToRun','bounds','Display','iter');
 
-for j = 1:10
-    
-    problem = createOptimProblem('fmincon','objective',...
-    @(x) Error(x,kdBruhns,mfiAdjMean4,mfiAdjMean26,v,biCoefMat,tnpbsa),'x0',zeros(7,1),...
-        'lb',(-10*ones(7,1)),'ub',10*ones(7,1),'options',opts);
-    [Rc(:,j), RcFit(j)] = run(gs,problem);
-        
-    %From all best fits from v = 1 to v = 10, find the value of v and its
-    %corresponding vector R which yield the least summed squared error
-    [~, IDX] = min(RcFit);
-    best = [Rc(:,IDX);IDX];
-    
-    %Results for MFI per flavor of receptor per flavor of immunoglobulin
-    %that we would expect based on our optimization
-    [~, mfiExp] = Error(best(1:size(best,1)-1),kdBruhns,mfiAdjMean4,mfiAdjMean26,best(8),biCoefMat,tnpbsa);
-end
-mfiDiff = [mfiExp(:,1:4) - mfiAdjMean4, mfiExp(:,5:8) - mfiAdjMean26];
+problem = createOptimProblem('fmincon','objective',...
+@(x) Error(x,kdBruhns,mfiAdjMean4,mfiAdjMean26,v,biCoefMat,tnpbsa),'x0',zeros(7,1),...
+    'lb',(-10*ones(7,1)),'ub',10*ones(7,1),'options',opts);
+[best, bsetFit, mfiExp] = run(gs,problem);
+
+mfiDiff = mfiExp - [mfiAdjMean4, mfiAdjMean26];
