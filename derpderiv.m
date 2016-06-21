3f507bca6e62b9e5667e993701998e37a174cdc9
rrobinet <rrobinet@mit.edu>
2016-06-21 09:04:22 -0400

    Cleaned up the repository; play4 is now MetropolisHastings, and play9 is now Hamiltonian. After fixing how AIC is calculated, MCMC works much, much better, with a good acceptance ratio. Error now outputs a concatenation of mfiExpPre4 and mfiExpPre26 (see code) which is necessary for calculating AICs. Also, VFinder helps find the pair of valencies which best fits the data.

--- derpderiv.m
+++
@@ -1,6 +0,0 @@
-function deriv = derpderiv(F,x0,epsilon)
-    if ~isscalar(x0)
-        error('Input must be scalar.')
-    end
-    deriv = (F(x0+epsilon) - F(x0-epsilon))/(2*epsilon);
-end
--- play13.m
+++
@@ -1,6 +0,0 @@
-function deriv = derpderiv(F,x0,epsilon)
-    if ~isscalar(x0)
-        error('Input must be scalar.')
-    end
-    deriv = (F(x+epsilon) - F(x-epsilon))/(2*epsilon);
-end
