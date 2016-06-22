a49f69430684d367262eb212ec254b2a343f8b33
Aaron Meyer <a@ameyer.me>
2016-06-22 14:42:11 -0400

    Fixed some errors
    Log probability was improperly defined and ReqFuncSolver failure wasn’t
    being caught.

--- PDF.m
+++ PDF.m
@@ -5,7 +5,7 @@ function [logprob] = PDF(x, kd, mfiAdjMean4, mfiAdjMean26, v, biCoefMat, tnpbsa,
     %given parameter fit for each combination of FcgR, IgG, and valency. It
     %is the concatenation of matrices mfiExpPre4 and mfiExpPre26; see
     %Error.m for their definiton
-    [J,mfiExp,mfiExpPre] = Error(x',kd,mfiAdjMean4,mfiAdjMean26,v,biCoefMat,tnpbsa);
+    [~,~,mfiExpPre] = Error(x',kd,mfiAdjMean4,mfiAdjMean26,v,biCoefMat,tnpbsa);
 
     
     %Check to see that for the parameter fit there exist expected values
@@ -18,19 +18,14 @@ function [logprob] = PDF(x, kd, mfiAdjMean4, mfiAdjMean26, v, biCoefMat, tnpbsa,
     %Create a matrix which includes the log probability of the model being
     %chosen for each combination of data from one FcgR, one IgG, and one
     %valency
-    logprobMat = zeros(6,8);
+    logprob = 0;
     for j = 1:6
         for k = 1:4
             for l = 1:2
-%                 logprobMat(j,4*(l-1)+k) = normlike([meanPerCond(4*(j-1)+k,l), ... 
-%                     stdPerCond(4*(j-1)+k,l)],mfiExpPre(j,4*(l-1)+k));
-                
-                logprobMat(j,4*(l-1)+k) = normpdf(mfiExpPre(j,4*(l-1)+k), ...
-                    meanPerCond(4*(j-1)+k,l),stdPerCond(4*(j-1)+k,l));
+                 logprob = logprob + normlike([meanPerCond(4*(j-1)+k,l), ... 
+                     stdPerCond(4*(j-1)+k,l)],mfiExpPre(j,4*(l-1)+k));
             end
         end
     end
     
-    %Sum up negative log probabilities and negate
-    logprob = -sum(sum(logprobMat));
 end
