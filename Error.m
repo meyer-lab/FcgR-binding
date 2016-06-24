e3f13536fff5f1f37d0d0454cf44baebb3850323
rrobinet <rrobinet@mit.edu>
2016-06-24 18:46:07 -0400

    Changed Error to take into account how much more MFI per TNP-BSA is emmitted by TNP-26-BSA than TNP-4-BSA. Also have MCMC running with 100000 samples, for no good reason. Please do not delete "play" files.

--- Error.m
+++ Error.m
@@ -1,21 +1,28 @@
 function [J, mfiExp, mfiExpPre] = Error(Rtot, Kd, mfiAdjMean, v, biCoefMat, tnpbsa)
     % If error is called with Rtot being a single value, assume we want to
     % have constant expression across all the receptors
-    if numel(Rtot) == 2
-        Rtot = [Rtot(1) * ones(6,1); Rtot(2)];
+    if numel(Rtot) == 3
+        Rtot = [Rtot(1) * ones(6,1); Rtot(2:3)];
     end
     %Convert from log scale
     Rtot = 10.^Rtot;
     Kx = Rtot(7);
     
-    %Get expected value of MFIs from Equation 7 from Stone
-    mfiExpPre = zeros(6,8);
+    %Get expected value of MFIs (before conversion factors) from Equation 7
+    %from Stone
+    mfiExpPrePre = zeros(6,8);
     for j = 1:6
         for k = 1:4
-            mfiExpPre(j,k) = StoneSolver(Rtot(j),Kx,v(1),Kd(j,k),tnpbsa(1),biCoefMat);
-            mfiExpPre(j,k+4) = StoneSolver(Rtot(j),Kx,v(2),Kd(j,k),tnpbsa(2),biCoefMat);
+            mfiExpPrePre(j,k) = StoneSolver(Rtot(j),Kx,v(1),Kd(j,k), ...
+                tnpbsa(1),biCoefMat);
+            mfiExpPrePre(j,k+4) = StoneSolver(Rtot(j),Kx,v(2),Kd(j,k), ...
+                tnpbsa(2),biCoefMat);
         end
     end
+    
+    %Multiply by conversion factors
+    mfiExpPre = mfiExpPrePre;
+    mfiExpPre(:,5:8) = Rtot(8)*mfiExpPrePre(:,5:8);
     
     %Check for undefined values (errors from ReqFuncSolver)
     if max(max(mfiExpPre == -1))
