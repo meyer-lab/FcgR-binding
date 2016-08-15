f5df5bf1d5c244339dae7942cffee7316a9a2edd
Ryan A. Robinett <rrobinet@mit.edu>
2016-08-15 15:47:39 -0400

    Wrote MATLAB scripts and a function which help find the number of significant local optima of our probability density function for the sake of running the EnsembleSampler more accurately.

---
+++ SpaceExplorerApp1.m
@@ -0,0 +1,27 @@
+clear;clc;
+
+load('SpaceExplorerData.mat')
+
+pstruct = struct;
+lnstruct = struct;
+len = (RissL-1)^7*(vissL-1)^2;
+for j = 1:len
+    clear current
+    high = -inf;
+    str = num2str(j);
+    for k = 1:len
+        temp = lntent{j};
+        if ~isempty(temp)
+            if temp > high
+                high = temp;
+                current = ptent{j};
+                ind = j;
+            end
+        end
+    end
+    eval(['pstruct.a' str ' = current;'])
+    eval(['lnstruct.a' str ' = high;'])
+    lntent{ind} = [];
+end
+
+save('SpaceExplorerApp1Data.mat','pstruct','lnstruct')
---
+++ recepnum1Connector1-2.py
@@ -0,0 +1,83 @@
+import matlab.engine
+from emcee import *
+import numpy
+import csv
+import time
+
+## Clear as csvs to be used
+fd = open('pos.csv','w')
+fd.write('')
+fd.close()
+
+fd = open('prob.csv','w')
+fd.write('')
+fd.close()
+
+#### Run simulation
+nsamples = int(input('Do you want to run the MCMC? If so, list how many \n' \
+                 + 'samples.\n'))
+## Start MATLAB engine
+## (It takes quite a while for the engine to be initialized)
+eng = matlab.engine.start_matlab()
+## Load necessary parameters for MATLAB functions
+eng.NormalErrorCnct()
+
+## Set up parameters for parallel-tempered Ensemble Sampler
+nwalkers = 100
+ndims = 12
+
+plength = nwalkers*ndims
+lnlength = nwalkers
+
+def logl(inp):
+    ## Data is input as a Numpy array; must convert to list in order
+    ## to input into MATLAB engine
+
+    Rtot = [];
+    for rtot in inp:
+        Rtot.append(rtot)
+    Rtot = matlab.double(Rtot)
+    logprob = eng.NormalErrorCnct(Rtot)
+    return logprob
+
+sampler = EnsembleSampler(nwalkers,ndims,logl,2.0,[],{},None,1,None,False,None)
+
+## Burn-in run
+p0 = numpy.random.rand(ndims*nwalkers).reshape((nwalkers,ndims))
+pos, prob, _ = sampler.run_mcmc(p0, 100)
+sampler.reset()
+
+## Start timer
+start = time.time()
+
+fdpos = open('pos.csv','a')
+fdprob = open('lnprob.csv','a')
+
+## Run sampler for nsamples many counts
+for j in range(nsamples):
+    pos, prob, _ = sampler.run_mcmc(p0,1)
+
+    posnew = pos.reshape(plength)
+    probnew = prob.reshape(lnlength)
+
+    posstore = ''
+    probstore = ''
+    
+    for elem in posnew:
+        posstore = posstore+','+str(elem)
+    for elem in probnew:
+        probstore = probstore+','+str(probnew)
+        
+    posstore = posstore+'\n'
+    probstore = probstore+'\n'
+
+    fdpos.write(posstore)
+    fdprob.write(probstore)
+    
+    sampler.reset()
+fdpos.close()
+fdprob.close()
+
+## End timer
+end = time.time()
+print('Finished. Time elapsed: '+str(end-start)+' seconds')
