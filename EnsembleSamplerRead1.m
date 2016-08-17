ee5c174995381f237e3c7a7e9d4d2d3c19c6e413
robbobbinett <rrobinet@mit.edu>
2016-08-17 16:20:26 -0400

    Updated script EnsembleSamplerRead1 to measure distribution ergodicity via autocorrelation. Deleted geweke, as it was unnecessary. Working on fixing a potential shortcoming of the recepnum1Connector .py files

--- EnsembleSamplerRead1.m
+++ EnsembleSamplerRead1.m
@@ -1,16 +1,55 @@
 clear;clc;
 
+disp('Gathering data...')
 % Load points from recepnum1Connector1-2.py. In the script, the number of
 % walkers is 100, and the number of dimensions is 12.
 temp = csvread('pos.csv');
+temp = temp(:,2:size(temp,2));
 nsamples = size(temp,1);
 nwalkers = 100;
 ndims = 12;
 
-% Create an array of points output from the sampler
-sample = zeros(nwalkers,ndims,nsamples);
-for j = 1:nsamples
-    sample(:,:,j) = reshape(temp(j,:),nwalkers,ndims);
-end
+% clc
+% disp('Plotting...')
+% for j = 1:nwalkers
+%     figure('Units','normalized','Position',[0 0 1 1])
+%     for k = 1:ndims
+%         subplot(3,4,k)
+%         autocorr(temp(:,ndims*(j-1)+k),20)
+%         title(num2str(j))
+%     end
+%     pause(2)
+%     close
+% end
 
-for 
+% Plot data
+sample = reshape(temp,ndims,nwalkers*nsamples);
+sample = sample';
+
+for j = 1:12
+    if j < 7
+        name = ['log Fc\gammaR No. ' num2str(j)];
+    elseif j == 7
+        name = 'log Kx Coefficient';
+    elseif j < 10
+        if j == 8
+            tnpbsa = 'TNP-4-BSA';
+        else
+            tnpbsa = 'TNP-26-BSA';
+        end
+        name = ['log Conversion Coefficient ' tnpbsa];
+    elseif j < 12
+        if j == 10
+            tnpbsa = 'TNP-4-BSA';
+        else
+            tnpbsa = 'TNP-26-BSA';
+        end
+        name = ['Avidity' tnpbsa];
+    else
+        name = 'log std Coefficient';
+    end
+    figure('units','normalized','position',[0 0 1 1])
+    plot(sample(:,j))
+    title(name)
+end
+    
--- PROPRNDTri.m
+++
@@ -1,4 +0,0 @@
-function next = PROPRNDTri(current,lbR,ubR,lbKx,ubKx,lbc,ubc,lbv,ubv,...
-    lbsigma,ubsigma,distR,distKx,distc,distsigma,percUni)
-
-    next
