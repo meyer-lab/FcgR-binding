d9ea2e1c70cb68a80b88b9e6cfb00526f6e448b6
Aaron Meyer <a@ameyer.me>
2016-12-11 10:12:15 -0500

    Integrated data loading into StoneModel class. Deduplicated code with normalizing function. Misc fixup.

--- StoneModel.py
+++ StoneModel.py
@@ -1,10 +1,11 @@
 import numpy as np
-from loadData import *
 from math import *
 from scipy.optimize import brentq
 from scipy.stats import norm
 from scipy.misc import comb
 from memoize import memoize
+import warnings
+from os.path import join
 
 nan = float('nan')
 inf = float('inf')
@@ -38,6 +39,49 @@ class StoneModel:
     @memoize
     def nchoosek(self, n, k):
         return comb(n, k, exact=True)
+
+    def normalizeData(self, filepath):
+        ## Read in the csv data for the first experiments. lux1 is an iterable data
+        ## structure wherein each iterable element is a single-element list containing a
+        ## string. Each such string represents a single row from the csv.
+        book4 = np.loadtxt(filepath, delimiter=',', skiprows=2, usecols=list(range(2,10)))
+
+        ## The first row in every set of five rows in book4 consists of background
+        ## MFIs. book5 is made by taking each of the four non-background MFIs from
+        ## reach cluster of 5 from book4, subtracting the corresponding background
+        ## MFI from each, and then forming a NumPy array of shape (24,8) from all of
+        ## these collectively. The final result, book5, will be a NumPy array of
+        ## shape (1,192).
+        book5 = np.array([])
+        for j in range(len(book4)):
+            if j%5 == 0:
+                temp = np.array(book4[j])
+            else:
+                temp2 = np.array(book4[j])-temp
+                book5 = np.concatenate((book5,temp2),0)
+        ## Reshape book5 into a NumPy array of shape (24,8), the actual shape of the
+        ## MFIs from Lux's original experiments.
+        book5 = np.reshape(book5,(24,8))
+        ## Transponse book5, so that all the elements in rows n and n+4 correspond to
+        ## the same replicate (for all n in {0,1,2,3}; Python indexing used). Then,
+        ## concatenate both rows in a single replicate, and take the mean of the
+        ## resulting array. This mean will correspond to the normalizing factor by
+        ## which the corresponding replicate is normalized. These are first contained
+        ## the (1,4) NumPy array means.
+        temp = np.transpose(book5)
+        means = [np.nanmean(np.concatenate((temp[j],temp[j+4]))) for j in range(4)]
+        ## Concatenate means with itself to result in a (1,8) NumPy array, means2
+        means2 = np.concatenate((means,means))
+        ## Concatenate means2 with itself until a NumPy array of shape (1,192) is
+        ## created. This array is titled "temp." Then, reshape temp into a (24,8)
+        ## NumPy array called "noise." Each element in book5 must be divided by the
+        ## corresponding element in temp in order to be normalized.
+        temp = means2
+        for j in range(book5.shape[0]-1):
+            temp = np.concatenate((temp,means2))
+        noise = np.reshape(temp,(24,8))
+        ## Create mfiAdjMean1 by dividing book5 by noise.
+        return(book5/noise)
 
     def StoneMod(self,logR,Ka,v,logKx,L0,fullOutput = False,skip=False):
         ## Returns the number of mutlivalent ligand bound to a cell with 10^logR
@@ -153,14 +197,60 @@ class StoneModel:
     # This should do the same as NormalErrorCoef above, but with the second batch of Nimmerjahn data and specified
     # Receptor expression levels
     def NormalErrorCoefRset(self, x):
-        Rvalues = np.log10(self.data['Rquant'])
-
-        return self.NormalErrorCoefcalc(np.concatenate((Rvalues, x)), self.data['mfiAdjMean2'], skip=True)
+        return self.NormalErrorCoefcalc(np.concatenate((self.Rquant, x)), self.mfiAdjMean2, skip=True)
 
     def NormalErrorCoef(self, x):
-        return self.NormalErrorCoefcalc(x, self.data['mfiAdjMean1'])
+        return self.NormalErrorCoefcalc(x, self.mfiAdjMean1)
 
     def __init__(self):
-        self.data = loadData()
-        self.kaBruhns = self.data['kaBruhns']
-        self.tnpbsa = self.data['tnpbsa']
+        ## Find path for csv files, on any machine wherein the repository recepnum1 exists.
+        path = './Nimmerjahn Lab and Bruhns Data'
+
+        ## Define the matrix of Ka values from Bruhns
+        ## For accuracy, the Python implementation of this code will use
+        ## Ka values as opposed to Kd, as these were the values which Bruhns
+        ## gives in his experiments. These are read in from a csv in the
+        ## folder Nimmerjahn Lab and Bruhns Data. Each row represents a particular
+        ## FcgR (FcgRIA, FcgRIIA-H, FcgRIIA-R, FcgRIIB, FcgRIIIA-F, FcgRIIIA-V)
+        ## and each column represents a particular IgG(1, 2, 3, 4).
+
+        ## First, read in the csv. It will result in an iterable object, wherein
+        ## each element is a single-element list containing a single string, each
+        ## string corresponding to a single row from the csv.
+        self.kaBruhns = np.loadtxt(join(path,'FcgR-Ka-Bruhns.csv'), delimiter=',')
+
+        ## Define concentrations of TNP-4-BSA and TNP-26-BSA, respectively
+        ## These are put into the numpy array "tnpbsa"
+        self.tnpbsa = np.array([1/67122,1/70928])*1e-3*5
+
+        ## Create the NumPy array Rquant, where each row represents a particular
+        ## IgG (1,2,3, or 4) and each column corresponds to a particular FcgR
+        ## (FcgRIA, FcgRIIA-H,FcgRIIA-R, FcgRIIB, FcgRIIIA-F, and FcgRIIIA-V)
+
+        ## Read in the receptor quantifications for the Nimmerjahn Lab's second
+        ## set of data. Using the function reader from the csv library, this data
+        ## is used to make the iterable object quant, each iterable element of
+        ## which is a single-element list containing a string corresponding to
+        ## a row in the original csv.
+        self.Rquant = np.loadtxt(join(path,'FcgRquant.csv'), delimiter=',', skiprows=1)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            self.Rquant = np.nanmean(self.Rquant, axis=0)
+
+        self.Rquant = np.log10(self.Rquant)
+
+        ## To begin, read in the MFI measurements from both of Lux's experiments from
+        ## their respective csvs. Then, subtract background MFIs from these nominal
+        ## MFIs. Then, normalize the data by replicate. For each step after the
+        ##reading, I manipulated the csv data in different ways, which are explained
+        ## in the comments. Please refer to these comments to understand what is
+        ## going on, especially with variables of the name "book$" or "temp$." All
+        ## variables with such names are only meant to construct mfiAdjMean1 (from
+        ## Lux's first experiments) and mfiAdjMean2 (from Lux's second experiments).
+
+        # Load and normalize dataset one
+        self.mfiAdjMean1 = self.normalizeData(join(path,'Luxetal2013-Fig2B.csv'))
+
+        # Load and normalize dataset two
+        self.mfiAdjMean2 = self.normalizeData(join(path,'New-Fig2B.csv'))
