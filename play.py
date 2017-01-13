bb209a7af11e750843f26cc06ddaf7de445b0d8c
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-13 16:20:46 -0500

    Successfully plotting mean-adjusted MFIs; need to add labels for TNP-4 and TNP-26-BSA. Trying to get rid of "zero bars" in middle of each subplot. Legend problem (sketchily) fixed.

--- play.py
+++ play.py
@@ -1,63 +1,84 @@
 import numpy as np
 import matplotlib.pyplot as plt
-##from matplotlib import rc
+from matplotlib import rc
 from StoneModel import StoneModel
 
-## Use LaTex to render text
-##rc('text',usetex=True)
+def mfiAdjMeanFigureMaker(newdata=True):
+    ## Use LaTex to render text; though usetex is input as False, it causes LaTex to be used.
+    ## Inputting usetex = True causes an error; this is a bug I found online
+    ## (http://matplotlib.1069221.n5.nabble.com/tk-pylab-and-unicode-td10458.html)
+    rc('text',usetex=False)
 
-## Our data
-StoneM1 = StoneModel(False)
-StoneM2 = StoneModel(True)
+    ## Our data
+    StoneM = StoneModel(newdata)
+    mfiAdjMean = StoneM.mfiAdjMean
 
-mfiAdjMean1 = StoneM1.mfiAdjMean
-mfiAdjMean2 = StoneM2.mfiAdjMean
-colors = ['red','blue','green','yellow']
-gam = chr(947)
-blankLabels = ['']*4
+    ## Number of bars, including an "empty" bar between TNP-4-BSA and TNP-26-BSA data
+    N = 9
 
-## Relative position of bars, of 9 bars
-ind = np.arange(4)
-## Width of bars
-width = 0.5
-## Number of bins per subplot
-N = 4
+    ## Setting up strings useful for plotting
+    colors = ['red','blue','green','yellow']
+    species = []
+    species.append(r'Fc$\gamma$RIA')
+    species.append(r'Fc$\gamma$RIIA-131R')
+    species.append(r'Fc$\gamma$RIIA-131H')
+    species.append(r'Fc$\gamma$RIIB')
+    species.append(r'Fc$\gamma$RIIIA-158F')
+    species.append(r'Fc$\gamma$RIIIA-158V')
+    blankLabels = ['']*N
 
-f, axarr = plt.subplots(2,3)
-graphs = []
-for j in range(6):
-    rects = []
-    for k in range(4):
-        temp = [0]*N
-        temp.remove(0)
-        temp.insert(k,np.nanmean(mfiAdjMean1[4*(j-1)+k][1:4]))
-        rects.append(axarr[int(np.floor(j/3)),j%3].bar(ind,temp,width,color=colors[k]))
-##    for k in range(4):
-##        temp = [0]*N
-##        temp.remove(0)
-##        temp.insert(5+k,np.nanmean(mfiAdjMean2[k][5:8]))
-##        rects.append(axarr[int(np.floor(j/3)),j%3].bar(ind,temp,width,color=colors[k]))
+    ind = np.arange(N)
+    ## Width of bars
+    width = 0.5
 
-# axes and labels
-for j in range(2):
-    for k in range(3):
-        axarr[j,k].set_xlim(-0.5*width,len(ind)-1+1.5*width)
-        axarr[j,k].set_xticklabels(blankLabels)
-        axarr[j,k].grid(b=False)
-        axarr[j,k].set_ylim(0,2)
-        if k%3 == 0:
-            axarr[j,k].set_ylabel('Mean-Adjusted MFIs')
-        axarr[j,k].set_title('Fc'+gam+'RIA')
-##xTickMarks = ['Group'+str(i) for i in range(1,6)]
-##ax.set_xticks(ind+width)
-##xtickNames = ax.set_xticklabels(xTickMarks)
-##plt.setp(xtickNames, rotation=45, fontsize=10)
-##
-#### add a legend
-leg = axarr[0,0].legend((rects[0][0],rects[1][0]),('Men','Women'),bbox_to_anchor=(1,1))
-##
-##wm = plt.get_current_fig_manager()
-##wm.window.state('zoomed')
+    ## Generate a figure with a 2 X 4 array of subplots; the rightmost column exists
+    ## only as a place to put the legend. The axes of these rightmost plots are whited
+    ## out for de-facto invisibility.
+    f, axarr = plt.subplots(2,4,figsize = (14,15))
 
-##plt.show()
-f.savefig('test.png')
+    ## Plotting mfiAdjMean
+    for j in range(6):
+        rects = []
+        for k in range(4):
+            temp = [0]*N
+            temp.remove(0)
+            temp.insert(k,np.nanmean(mfiAdjMean[4*(j-1)+k][1:4]))
+            rects.append(axarr[int(np.floor(j/3)),int(j+np.floor(j/3))%4].bar(ind,temp,width,color=colors[k]))
+        
+        for k in range(4):
+            temp = [0]*N
+            temp.remove(0)
+            temp.insert(5+k,np.nanmean(mfiAdjMean[4*(j-1)+k][5:8]))
+            rects.append(axarr[int(np.floor(j/3)),int(j+np.floor(j/3))%4].bar(ind,temp,width,color=colors[k]))
+
+    # axes and labels
+    for j in range(2):
+        for k in range(3):
+            axarr[j,k].set_xlim(-0.5*width,len(ind)-1+1.5*width)
+            axarr[j,k].set_xticklabels(blankLabels)
+            axarr[j,k].grid(b=False)
+            axarr[j,k].set_ylim(0,5)
+            if k%3 == 0:
+                axarr[j,k].set_ylabel('maMFIs',fontsize=14)
+            axarr[j,k].set_title(species[3*j+k],fontsize=16)
+
+    ## Add a legend denoting IgG species
+    leg = axarr[0,3].legend((rects[i][0] for i in range(4)),('IgG'+str(i+1) for i in range(4)),bbox_to_anchor=(0.5,1))
+    ## White out the rightmost set of aubplots
+    for j in range(2):
+        axarr[j,3].spines['bottom'].set_color('white')
+        axarr[j,3].spines['top'].set_color('white') 
+        axarr[j,3].spines['right'].set_color('white')
+        axarr[j,3].spines['left'].set_color('white')
+        axarr[j,3].tick_params(axis='x', colors='white')
+        axarr[j,3].tick_params(axis='y', colors='white')
+
+    ## Set title for the set of plots
+    ##titleEnd = ' (New Data)'
+    f.suptitle('Mean-Adjusted MFIs (Original Data)',fontsize=18)
+    ## Show figure
+    plt.show()
+    ## Save figure as image
+    f.savefig('mfiAdjMean1.png')
+
+mfiAdjMeanFigureMaker(False)
