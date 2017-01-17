2214ac3e82e7c69558dca9d18522acb7829c522a
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-16 20:04:07 -0500

    READY FOR MERGE. Altered bar graph title, set up y-axis label, took the code from temp.py and put it into a function in StoneHelper.py.

--- StoneHelper.py
+++ StoneHelper.py
@@ -331,3 +331,49 @@ def mfiAdjMeanFigureMaker(StoneM):
 
     ## Show figure
     plt.show()
+
+def FcgRQuantificationFigureMaker(StoneM):
+    ## Please see comments from mfiAdjMeanFigureMaker
+    rc('text',usetex=False)
+
+    Rquant = StoneM.Rquant
+
+    ## Number of bars
+    N = len(Rquant)
+    ## Relative width of bars
+    width = 0.5
+    ## List of 6 tuples, one tuple per FcgR species. The tuple's first element is the
+    ## (nan)mean expression for that species, while the second element is the (nan)std
+    ## of the distribution of receptor expressions measured
+    iterable = [(np.nanmean(10**Rquant[j]),np.nanstd(10**Rquant[j])) for j in range(N)]
+
+    ## Set up strings necessary for the coloring and the legend
+    ind = np.arange(N)
+    colors = ['red','orange','yellow','green','blue','purple']
+    species = ['IA','IIA-131R','IIA-131H','IIB','IIIA-158F','IIIA-158V']
+
+    ## Create bars and error bars per species
+    f = plt.figure()
+    ax = f.add_subplot(121)
+    rects = []
+    for j in range(N):
+        temp = [0]*(N-1)
+        temp.insert(j,iterable[j][0])
+        stds = [0]*(N-1)
+        stds.insert(j,iterable[j][1])
+        rects.append(ax.bar(ind,temp,color=colors[j],yerr=stds,error_kw=dict(elinewidth=2,ecolor='black')))
+
+    ## Set up axes
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(-0.5*width,len(ind)+0.2*width)
+    ax.tick_params(axis='x',length=0)
+    ax.grid(b=False)
+    ax.set_yscale('log')
+    ax.set_ylabel('Number of Receptors',fontsize=14)
+    f.suptitle('Receptor Quantification by Receptor Species',fontsize=18)
+
+    ## Create legend
+    leg = ax.legend((rects[j][0] for j in range(N)),(r'Fc$\gamma$R'+species[j] for j in range(N)),bbox_to_anchor=(2,1))
+
+    ## Show figure
+    plt.show()
