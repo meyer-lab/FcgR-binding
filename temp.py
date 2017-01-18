d0d7719add64f996e1f424075ff59b6440b0ffde
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-18 18:11:30 -0500

    Changed the title of the axes made by FcgRQuantificationFigureMaker to two lines; temp.py creates a PDF housing Figure 1, missing only a title for the mean-adjusted MFI graphs. temp2.py was created in order to make the PDF for Figure 2.

--- temp.py
+++ temp.py
@@ -2,17 +2,22 @@ import matplotlib.pyplot as plt
 from StoneModel import StoneModel
 from StoneHelper import *
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import gridspec
 import numpy as np
 
 StoneM = StoneModel()
 
 f = plt.figure(figsize=(20,20))
-ax = f.add_subplot(221)
-FcgRQuantificationFigureMaker(StoneM,ax)
+gs1 = gridspec.GridSpec(2,2,height_ratios=[1,3],width_ratios=[1,3])
+print(gs1)
+ax = f.add_subplot(gs1[0])
+FcgRQuantificationFigureMaker(StoneM,ax,legbbox=(1.75,1),titlefontsize=16)
+gs2 = gridspec.GridSpec(8,4,height_ratios=[1,1,1,1,1,4,1,4])
 axarr = []
 for j in range(6):
-    exec('axarr.append(f.add_subplot(8,4,'+str(int(25+j+np.floor(j/3)))+'))')
-mfiAdjMeanFigureMaker(StoneM,axarr)
+##    exec('axarr.append(f.add_subplot(8,4,'+str(int(25+j+np.floor(j/3)))+'))')
+    axarr.append(f.add_subplot(gs2[20+j+5*int(np.floor(j/3))]))
+mfiAdjMeanFigureMaker(StoneM,axarr,legbbox=(1.5,1),tnpbsafontsize=12)
 ##plt.show()
 
 with PdfPages('test.pdf') as pdf:
