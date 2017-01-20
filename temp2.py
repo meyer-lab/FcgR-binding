0aa992f8984bdc222c453495054ecb9936aab1f1
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-20 10:19:28 -0500

    trivial progress

--- temp2.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from StoneModel import StoneModel
-from StoneHelper import *
-from matplotlib.backends.backend_pdf import PdfPages
-from matplotlib import gridspec
-import numpy as np
-
-StoneM = StoneModel()
