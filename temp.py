f1f4a2d7cc2ef02a4dc02eef1654da8b537a04ce
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-25 17:02:38 -0500

    Simplified the map and reduce steps by writing the functions mapStore and reduce in StoneHelper; mapStore stores the output dataFrame as a .pkl file. reduce loads this .pkl file and outputs the reduced dataset. The .gitignore now ignores .pkl files. There is a formatting problem recognized by the seaborn API that I am working on fixing to make Meyer's scatterplots turn out correctly.

--- temp.py
+++ temp.py
@@ -4,30 +4,6 @@ import matplotlib.colors as mcolors
 import numpy as np
 from StoneHelper import *
 
-
-M, dset = read_chain('mcmc_chain.h5')
-
-print(type(dset.as_matrix()))
-
-bestIDX = np.argmax(dset['LL'])
-
-p = dset.iloc[bestIDX,:][2:].as_matrix()
-##print(list(dset[2]))
-# Trim the burn in period
-##dset = dset.iloc[300::,:]
-
-dsett = dset.sort_values(by = 'LL')
-
-##print(dset[[list(dset)[j] for j in range(7,len(list(dset)))]])
-##dd = getFitPrediction(M,dset[[list(dset)[j] for j in range(7,len(list(dset)))]])
-
-
-##print(dset.columns)
-
-##dset = dset.assign(sigDiff = lambda x: np.power(10, x.sigConv2 - x.sigConv1))
-##dset = dset.assign(gnuDiff = lambda x: x.gnu2 / x.gnu1)
-##
-##temp = getFitPrediction(M,dset)
-frameList = mapMCMC(lambda x: getFitPrediction(M,x),dset.as_matrix()[:,2:])
-frameAgg = reduceMCMC(frameList)
-np.save('frameAgg',frameAgg)
+##mapStore()
+frameList = reduce()
+print(frameList)
