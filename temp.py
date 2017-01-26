9a6b39cf52885ddcb91ec2bbee775f5b94b84f80
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-25 22:42:58 -0500

    Working on over-correcting for some problems caused by using seaborn color palettes in matplotlib--the problems persist even if only importing the colors, and not seaborn

--- temp.py
+++ temp.py
@@ -1,9 +1,13 @@
-import seaborn as sns
+##import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import numpy as np
 from StoneHelper import *
 
-##mapStore()
-frameList = reduce()
-print(frameList)
+x = np.arange(200)
+plt.plot(x,x)
+plt.show()
+
+seaborn_colorblindSteal()
+##Colors = sns.color_palette('colorblind')
+##print(type(Colors[1][1]))
