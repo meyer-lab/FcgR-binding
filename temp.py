3b7486ea212c1bc91fb5f0de32ae1a4b7d0ee175
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-26 10:03:54 -0500

    Fixed the major issue with the fit line plot (most points in scatterplot were not even visible)

--- temp.py
+++ temp.py
@@ -4,10 +4,5 @@ import matplotlib.colors as mcolors
 import numpy as np
 from StoneHelper import *
 
-x = np.arange(200)
-plt.plot(x,x)
+plotFit()
 plt.show()
-
-seaborn_colorblindSteal()
-##Colors = sns.color_palette('colorblind')
-##print(type(Colors[1][1]))
