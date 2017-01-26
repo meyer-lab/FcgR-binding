ab94d61993fd2ec226b756b88f8711f226f1133f
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-26 15:59:07 -0500

    Finished Rexp boxplot, save ytick labels. Going back over plotting functions in StoneHelper and inserting axes arguments/options where lacking; working on setting default background color to be the seaborn background color in all these plots.

--- temp.py
+++ temp.py
@@ -4,5 +4,9 @@ import matplotlib.colors as mcolors
 import numpy as np
 from StoneHelper import *
 
-plotFit()
-plt.show()
+##f = plt.figure(figsize=(9,5))
+##ax = plt.gca()
+##pseudoHist(ax)
+
+##histSubplots()
+sigmaNuHists()
