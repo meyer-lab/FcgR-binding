78f7c1aa4de155238a41a858ae4e98a79476c89f
Ryan A. Robinett <rrobinet@mit.edu>
2017-01-26 09:26:23 -0500

    (Believe that I've) fixed the formatting problem with the bindingvsKA scatterplot w/ errorbars

--- Figure1.py
+++ Figure1.py
@@ -18,10 +18,10 @@ import numpy as np
 ####import os.path
 ####sys.path.append(
 ####    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-from StoneModel import StoneModel
+import StoneModel
 from StoneHelper import *
 
-StoneM = StoneModel()
+StoneM = StoneModel.StoneModel()
 
 f = plt.figure(figsize=(20,20))
 gs1 = gridspec.GridSpec(2,3,height_ratios=[1,3],width_ratios=[2,3,3])
