204e006feeece4eca50639b275e17ac631c45786
nguan <ningguan@mit.edu>
2017-02-02 15:34:39 -0500

    Added Pandas Tables

--- StoneModMouse.py
+++ StoneModMouse.py
@@ -1,5 +1,6 @@
 import StoneModel
 import numpy as np
+import pandas as pd
 from scipy.optimize import brentq
 from scipy.misc import comb
 from os.path import join
@@ -28,18 +29,20 @@ class StoneModelMouse:
         ## equations derived from Stone et al. (2001).
 
         # Assign Ig type to a number corresponding to the row of Ka
+        x1 = x[:]
         for i in range(4):
             if self.Igs[i] == x[self.IgIDX]:
-                x[self.IgIDX] = i
-        if type(x[self.IgIDX]) != int:
-            return (np.nan, np.nan, np.nan)
+                x1[self.IgIDX] = np.nan
+                l = i
+#        if type(x[self.IgIDX]) != int:
+#            return (np.nan, np.nan, np.nan)
+        #print(x[self.IgIDX])
 
         # Assign inputs for StoneMod
-        x = np.array(x)
-        l = int(x[self.IgIDX])
-        v = x[self.uvIDX]
-        Kx = np.power(10, x[self.kxIDX])
-        L0 = x[self.L0IDX]
+        x1 = np.array(x1)
+        v = x1[self.uvIDX]
+        Kx = np.power(10, x1[self.kxIDX])
+        L0 = x1[self.L0IDX]
 
         # Initiate numpy arrays for StoneMod outputs
         outputLbnd = np.full((6), np.nan)
@@ -52,10 +55,10 @@ class StoneModelMouse:
 
         # Iterate over each FcgR
         for k in range(6):
-            logR = x[k]
+            logR = x1[k]
             ## Set the affinity for the binding of the FcgR and IgG in question
             Ka = self.kaMouse[k][l]
-            if Ka == '+' or Ka == 0:
+            if Ka == '+' or Ka == 0 or np.isnan(Ka):
                 continue
             Ka = float(Ka)
             ## Calculate the MFI which should result from this condition according to the model
@@ -72,3 +75,64 @@ class StoneModelMouse:
         if fullOutput:
             return (outputLbnd, outputRbnd, outputRmulti, outputnXlink, outputReq)
         return (outputLbnd, outputRbnd, outputReq)
+		
+    def pdOutputTable(self, x, fullOutput = False):
+        # Organizes the binding prediction between the 24 Ig-FcgR pairs calculated by StoneModMouse(x)
+        # Outputs a pandas table of binding prediction
+        stoneModMurine = []
+        labels = []
+
+        # Set labels for columns of pandas table
+        if fullOutput:
+            for i in self.FcgRs:
+                for j in ['-Lbnd', '-Rbnd', '-Rmulti', '-nXlink', '-Req']:
+                    labels.append(i+j)
+        else:
+            for i in self.FcgRs:
+                for j in ['-Lbnd', '-Rbnd', '-Req']:
+                    labels.append(i+j) 
+                    
+        # Make a 3-d array of StoneModMouse output for each Ig
+        if fullOutput:
+            for i in range(len(self.Igs)):
+                x[self.IgIDX] = self.Igs[i]
+                stoneModMurine.append(np.transpose(self.StoneModMouse(x, fullOutput = True)))
+        else:
+            for i in range(len(self.Igs)):
+                x[self.IgIDX] = self.Igs[i]
+                stoneModMurine.append(np.transpose(self.StoneModMouse(x)))
+
+        # Reshape data for pandas table
+        output = np.array(stoneModMurine)
+        if fullOutput:
+            output = np.reshape(output,(4,30))
+        else:
+            output = np.reshape(output,(4,18))
+        
+        # Make pandas table of binding predictions of Ig-FcgR pairs
+        table = pd.DataFrame(np.array(output), index = self.Igs, columns = labels)
+        return table
+    
+    def pdAvidityTable(self, x, vl, vu, fullOutput = False):
+        # Takes in a list of shape (9) for x, lower bond for avidity vl, and upper bond for avidity vu
+        # Organizes a pandas table of binding predictions for a given Ig as avidity varies
+        tb1 = pd.DataFrame()
+        Ig = x[self.IgIDX]
+        idx = []
+        # Concatenating a pandas table for a range of avidity 
+        if fullOutput == False:
+            for i in range(vl, vu+1):
+                x[self.uvIDX] = i
+                tb = self.pdOutputTable(x, fullOutput = False)
+                tb1 = pd.concat([tb1, tb.loc[[Ig]]])
+        elif fullOutput == True:
+            for j in range(vl, vu+1):
+                x[self.uvIDX] = j
+                tb = self.pdOutputTable(x, fullOutput = True)
+                tb1 = pd.concat([tb1, tb.loc[[Ig]]])
+        # Indexing
+        for k in range(vl, vu+1):
+            idx.append(Ig+'-'+str(k))
+        tb1.index = idx
+        return tb1
+            
