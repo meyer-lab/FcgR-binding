5c094606b5a0447db50c3457960cae97a01bcfe2
Aaron Meyer <a@ameyer.me>
2017-02-02 16:18:23 -0500

    (Don’t need pickle in the sampler.)

--- Sampler.py
+++ Sampler.py
@@ -3,11 +3,6 @@ import numpy as np
 from recepmod import StoneModel
 from recepmod.fitFuncs import *
 
-try:
-   import cPickle as pickle
-except:
-   import pickle
-
 newData = True
 bestLL = -np.inf
 
