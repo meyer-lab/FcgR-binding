c201b877f164ed369e4dcc1adee86873848bacad
Aaron Meyer <a@ameyer.me>
2017-02-03 19:25:38 -0500

    Writing (#27)
    * Some refinement for the varied Kx model.
    
    * Lots of writing adjustments.
    * Finished some updates. Writing is really waiting on figure assembly.
    * Fixed building.
    * Pulled pandoc-crossref to fix building, and reconnected pieces for KxVar model.
    * (Don’t need pickle in the sampler.)

---
+++ Manuscript/Text/03_Discussion.md
@@ -0,0 +1,14 @@
+# Discussion
+
+
+
+
+
+
+
+
+
+### A unified model for FcgR engagement will enable further refinement for the function of these receptors
+  - Examining interaction of multiple antibodies
+  - More precisely engineered glycosylation variants of Fc region
+  - Improved systems view of effector function
--- SamplerVarKx.py
+++ SamplerVarKx.py
@@ -1,13 +1,13 @@
 from emcee import EnsembleSampler
 import numpy as np
-from recepmod import StoneModel
+from recepmod import StoneModelVarKx
 from recepmod.fitFuncs import *
 
 newData = True
 bestLL = -np.inf
 
 ## Load model
-StoneM = StoneModel.StoneModel(newData)
+StoneM = StoneModelVarKx.StoneModel(newData)
 
 #### Run simulation
 niters = 100000
@@ -18,7 +18,7 @@ p0, ndims, nwalkers = getUniformStart(StoneM)
 ## Set up sampler
 sampler = EnsembleSampler(nwalkers,ndims,StoneM.NormalErrorCoef,2.0,[],{},None,16)
 
-f, dset = startH5File(StoneM, "mcmc_chain.h5")
+f, dset = startH5File(StoneM, "var_mcmc_chain.h5")
 thinTrack = 0
 thin = 200
 
