951c83a49c35e6c835fc8076e0e166f6a813e709
Aaron Meyer <a@ameyer.me>
2017-02-07 15:15:39 -0500

    Aaron figure adjustments (#29)
    * Update gitignore and include testing chain.
    * Enable generation of Figure 2.
    * Silence output for mouse Pandas test.
    * Don’t plot Figure 1 when generating.
    * Reorganize and plot Figure 2.
    * Test Figure 2.
    * Add placeholder Figure 2.

--- recepmod/figures/Figure2.py
+++ recepmod/figures/Figure2.py
@@ -5,6 +5,38 @@ import matplotlib.pyplot as plt
 from ..StoneModel import StoneModel
 from ..StoneHelper import *
 from .FigureCommon import *
+import os
+
+def makeFigure():
+    # Retrieve model and fit from hdf5 file
+    M, dset = read_chain(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/mcmc_chain.h5"))
+
+    pBest = dset.iloc[np.argmax(dset['LL']),:][2:].as_matrix()
+
+    # Setup plotting space
+    f = plt.figure(figsize=(7,5))
+
+    # Make grid
+    gs1 = gridspec.GridSpec(3,3)
+    ax1 = f.add_subplot(gs1[0])
+
+    LLplot(dset, f.add_subplot(gs1[1]))
+
+    ax4 = f.add_subplot(gs1[3])
+    ax5 = f.add_subplot(gs1[4])
+    ax6 = f.add_subplot(gs1[5])
+    ax7 = f.add_subplot(gs1[6])
+    ax8 = f.add_subplot(gs1[7])
+    ax9 = f.add_subplot(gs1[8])
+
+    plotFit(getFitMeasMergedSummarized(M, pBest), ax = f.add_subplot(gs1[2]))
+
+
+    histSubplots(dset, axes = [ax4, ax5, ax6, ax7])
+
+    violinPlot(dset, ax = ax8)
+
+    return f
 
 def plotQuant(fitMean, nameFieldX, nameFieldY, ax=None, backGray=True, legend=True):
     # This should take a merged and summarized data frame
@@ -38,103 +70,40 @@ def plotQuant(fitMean, nameFieldX, nameFieldY, ax=None, backGray=True, legend=Tr
     ax.set_xscale('log')
     plt.ylabel(nameFieldY)
     plt.xlabel(nameFieldX)
-    if backGray:
-        ax.set_facecolor(backColor)
 
-def LLscatter(ax=None,backGray=True):
+def violinPlot(dset, ax=None):
+    # If no axis was provided make our own
     if ax == None:
         ax = plt.gca()
-    dset.plot('LL', 'gnu2', 'scatter',ax=ax)
-    if backGray:
-        ax.set_facecolor(backColor)
-    plt.show()
 
-def gnuScatter(ax=None,backGray=True):
-    if ax == None:
-        ax = plt.gca()
-    dsett.plot(x = 'gnu1', y = 'gnu2', kind = 'scatter', c = 'LL', s = 50,ax=ax)
-    plt.xlabel('gnu1')
-    if backGray:
-        ax.set_facecolor(backColor)
-    plt.show()
 
-def violinTest(ax=None):
-    if ax == None:
-        ax = plt.gca()
     for j in range(len(FcgRs)):
         toplot = np.full(dset[['Rexp']].values.shape,0.0)
         toplot[:][j] = dset[['Rexp']].values[:][j]
         objs = sns.violinplot(data=toplot,color=colors[0],ax=ax)
+
+    print(toplot)
+
     ax.set_xticks([])
-    ax.semilogy()
-    print(objs)
-    plt.show()
+    ax.set_facecolor('white')
 
-def sigmaNuHists(axes=None,tight_layout=False,backGray=True):
-    if axes == None:
-        fig, axes = plt.subplots(nrows=2, ncols=1)
-    newdset = dset
-    newdset = newdset.assign(sigDiff = lambda x: np.power(10, x.sigConv2 - x.sigConv1))
-    newdset = newdset.assign(gnuDiff = lambda x: x.gnu2 / x.gnu1)
-    newdset['sigDiff'].plot.hist(ax=axes[0], bins = 100).set_xlabel(r'$\frac{\sigma^*_{26}}{\sigma^*_4}$',fontsize=16)
-    newdset['gnuDiff'].plot.hist(ax=axes[1], bins = 20).set_xlabel(r'$\frac{\nu_{26}}{\nu_4}$',fontsize=16)
-    if backGray:
-        for ax in axes:
-            ax.set_facecolor(backColor)
-    if tight_layout:
-        plt.tight_layout()
-    plt.show()
-
-def histSubplots(axes=None,tight_layout=False,backGray=True):
-    if axes == None:
-        fig, axes = plt.subplots(nrows=3, ncols=2)
-
-    dset[['Kx1'     ]].plot.hist(ax=axes[0,0], bins = 100, color=colors[0])
-    dset[['sigConv1', 'sigConv2']].plot.hist(ax=axes[0,1], bins = 100, color=[colors[j] for j in range(2)])
-    dset[['gnu1', 'gnu2'        ]].plot.hist(ax=axes[1,0], bins = 100, color=[colors[j] for j in range(2)])
-    dset[['sigma', 'sigma2'     ]].plot.hist(ax=axes[1,1], bins = 100, color=[colors[j] for j in range(2)])
-
-    RexpBoxplot(axes[2,0])
-
-##    temp = dset[['Rexp']].values
-##    for j in range(temp.shape[1]):
-##       y = np.array([-j+5]*temp.shape[0])
-##    axes[2,0].set_ylim(0,12000)
-##    print(good)
-    a = 1e3
-    b = 1e5
-    c = 1e3
-    d = 1e5
-##    h, l = axes[2,0].get_legend_handles_labels()
-##    h[0].get_bbox().set_points([[a,b],[c,d]])
-    ##print(h[0].get_bbox().get_points())
-
-    if tight_layout:
-        plt.tight_layout()
-    if backGray:
-        for elem in axes:
-            for ax in elem:
-                ax.set_facecolor(backColor)
-    plt.show()
-
-#fig = plt.figure((8.5,11))
-
-def RexpBoxplot(ax=None):
+def LLplot(dset, ax = None):
     if ax == None:
         ax = plt.gca()
-    objs = ax.boxplot(10**dset[['Rexp']].as_matrix(),whis=[5,95],sym='')
-    for j in range(int(len(objs['caps'])/2)):
-        objs['caps'][2*j].set_color(FcgRs[fcgrs[j]])
-        objs['caps'][2*j+1].set_color(FcgRs[fcgrs[j]])
-        objs['boxes'][j].set_color(FcgRs[fcgrs[j]])
-        objs['medians'][j].set_color(FcgRs[fcgrs[j]])
-        objs['whiskers'][2*j].set_color(FcgRs[fcgrs[j]])
-        objs['whiskers'][2*j+1].set_color(FcgRs[fcgrs[j]])
-        for elem in Rquant[j]:
-            ax.plot(j+1,10**elem,color=FcgRs[fcgrs[j]],marker='x')
-    ax.semilogy()
-    ax.set_xticks([])
-    ax.set_ylabel(ylabel='Number of Receptors Expressed')
+
+    plt.plot(dset['LL'], axes = ax)
+
+def histSubplots(dset, axes=None):
+    if axes == None:
+        fig, axes = plt.subplots(nrows=1, ncols=4)
+
+    dset[['Kx1']].plot.hist(ax=axes[0], bins = 100, color=colors[0])
+    dset[['sigConv1', 'sigConv2']].plot.hist(ax=axes[1], bins = 100, color=[colors[j] for j in range(2)])
+    dset[['gnu1', 'gnu2']].plot.hist(ax=axes[2], bins = 100, color=[colors[j] for j in range(2)])
+    dset[['sigma', 'sigma2']].plot.hist(ax=axes[3], bins = 100, color=[colors[j] for j in range(2)])
+
+    for x in axes:
+        x.set_facecolor('white')
 
 def plotFit(fitMean,ax=None, backGray=True):
     # This should take a merged and summarized data frame
