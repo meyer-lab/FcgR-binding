33b4abce6aa3b1f8905d2e08ac8c35b7f5ddde39
Aaron Meyer <a@ameyer.me>
2017-02-07 15:15:39 -0500

    Aaron figure adjustments (#29)
    * Update gitignore and include testing chain.
    * Enable generation of Figure 2.
    * Silence output for mouse Pandas test.
    * Don’t plot Figure 1 when generating.
    * Reorganize and plot Figure 2.
    * Test Figure 2.
    * Add placeholder Figure 2.

--- recepmod/tests/test_Mouse.py
+++ recepmod/tests/test_Mouse.py
@@ -14,10 +14,10 @@ class TestStoneMouse(unittest.TestCase):
     def tearDown(self):
         t = time.time() - self.startTime
         print("%s: %.3f" % (self.id(), t*1000))
-    
+
     def test_dataImport_kaMouse(self):
         self.assertTrue(self.Mod.kaMouse.shape == (6,4))
-    
+
     def test_dataOutput_StoneModMouse(self):
         # Checks size of fullOutput
         logR = np.log10(30000*random.random())
@@ -29,7 +29,7 @@ class TestStoneMouse(unittest.TestCase):
         x = [logR, logR, logR, logR, logR, logR, 'IgG1', kx, v, Li]
         out = np.array(self.Mod.StoneModMouse(x, fullOutput = True))
         self.assertTrue(out.shape == (5,6))
-        
+
     def test_dataOutput_StoneModMouse2(self):
         # Checks that the model output satisfies R = Rbnd + Req
         logR = np.log10(30000*random.random())
@@ -45,7 +45,7 @@ class TestStoneMouse(unittest.TestCase):
         for j in range(5):
             if not np.isnan(b[j]):
                 self.assertAlmostEqual(10**logR, b[j], delta = (10**logR)/10000)
-                
+
     def test_pdOutputTable(self):
         # Checks the shape of pandas table from pdOutputTable
         logR = np.log10(30000*random.random())
@@ -59,7 +59,7 @@ class TestStoneMouse(unittest.TestCase):
         self.assertTrue(tb.shape == (4,18))
         tbfull = self.Mod.pdOutputTable(x, fullOutput = True)
         self.assertTrue(tbfull.shape == (4,30))
-    
+
     def test_pdAvidityTable(self):
         # Check shape of pandas table from pdAvidityTable
         logR = np.log10(30000*random.random())
@@ -74,7 +74,7 @@ class TestStoneMouse(unittest.TestCase):
         x2 = [logR, logR, logR, logR, logR, logR, 'IgG2a', kx, v, Li]
         tba2 = self.Mod.pdAvidityTable(x2, v, v+2, fullOutput = True)
         self.assertTrue(tba2.shape == (3,30))
-    
+
     def test_pdAvidityTable2(self):
         logR = np.log10(10**5)
         kx = 10**(-7)
@@ -84,8 +84,8 @@ class TestStoneMouse(unittest.TestCase):
             raise ValueError('Negative input parameters')
         x = [logR, logR, logR, logR, logR, logR, 'IgG1', kx, v, Li]
         tbN = self.Mod.NimmerjahnEffectTable(x)
-        print(tbN.iloc[:, list(range(10,20))])
+        #print(tbN.iloc[:, list(range(10,20))])
         self.assertTrue(tbN.shape == (8,31))
-        
+
 if __name__ == '__main__':
     unittest.main()
