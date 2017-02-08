0b096f6c74cf71e40fab0c6375187ec2a882e20b
Aaron Meyer <a@ameyer.me>
2017-02-08 11:37:27 -0500

    Generate figures dynamically.

--- genFigures.py
+++ genFigures.py
@@ -6,8 +6,8 @@ from recepmod.figures import Figure3
 from recepmod.figures import Figure4
 from recepmod.figures import Figure5
 
-with PdfPages('Figure1.pdf') as pdf:
+with PdfPages('./Manuscript/Figures/Figure1.pdf') as pdf:
     pdf.savefig(Figure1.makeFigure())
 
-with PdfPages('Figure2.pdf') as pdf:
+with PdfPages('./Manuscript/Figures/Figure2.pdf') as pdf:
     pdf.savefig(Figure2.makeFigure())
