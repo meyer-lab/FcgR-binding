d9c9394d3c38a17e3296f6ba60df77fc2a113164
Ryan A. Robinett <rrobinet@mit.edu>
2017-02-08 03:06:32 -0500

    Forgot to reset genFigures.py

--- genFigures.py
+++ genFigures.py
@@ -9,5 +9,5 @@ from recepmod.figures import Figure5
 with PdfPages('Figure1.pdf') as pdf:
     pdf.savefig(Figure1.makeFigure())
 
-##with PdfPages('Figure2.pdf') as pdf:
-##    pdf.savefig(Figure2.makeFigure())
+with PdfPages('Figure2.pdf') as pdf:
+    pdf.savefig(Figure2.makeFigure())
