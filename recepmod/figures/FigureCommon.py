61a028fc63fe1a7a8f27b6f87ca942bccd6aae26
Ryan A. Robinett <rrobinet@mit.edu>
2017-02-08 03:05:15 -0500

    Figure 1 complete, save a small number of aesthetic issues and perhaps subfigure labellings; otherwise, ready to show other lab

--- recepmod/figures/FigureCommon.py
+++ recepmod/figures/FigureCommon.py
@@ -11,22 +11,28 @@ from matplotlib.font_manager import FontProperties
 #from tqdm import tqdm
 ##import seaborn as sns
 
+##plt.rc('text',usetex=True)
+
 def seaborn_colorblindGet():
    # This function collects the collor palette settings used in seaborn-colorblind, so as
    # to get all of seaborn's colors without the unwanted formatting effects of seaborn
    # backColor is the background color used in seaborn's colorblind setting
-   Colors = pd.read_pickle('seaborn_colorblindColors.pkl').values
+##   Colors = pd.read_pickle('seaborn_colorblindColors.pkl').values
+   Colors = pd.read_pickle('seaborn_colorblindColors2.pkl').values
    backColor = tuple([Colors[j][0] for j in range(3)])
    colors = []
    for j in range(1,7):
        colors.append(tuple([Colors[k][j] for k in range(3)]))
-   return colors, backColor
+   colors2 = []
+   for j in range(7,11):
+       colors2.append(tuple([Colors[k][j] for k in range(3)]))
+   return colors, backColor, colors2
 
-colors, backColor = seaborn_colorblindGet()
+colors, backColor, colors2 = seaborn_colorblindGet()
 
 Igs = {'IgG1':'o', 'IgG2':'d', 'IgG3':'s', 'IgG4':'^'}
 
-fcgrs = ['FcgRI','FcgRIIA-131R','FcgRIIA-131H','FcgRIIB','FcgRIIIA-158F','FcgRIIIA-158V']
+fcgrs = [r'Fc$\gamma$RI',r'Fc$\gamma$RIIA-131R',r'Fc$\gamma$RIIA-131H',r'Fc$\gamma$RIIB',r'Fc$\gamma$RIIIA-158F',r'Fc$\gamma$RIIIA-158V']
 FcgRs = {}
 for j in range(len(fcgrs)):
     FcgRs[fcgrs[j]] = colors[j]
