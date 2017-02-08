7019293b19eb92f237231f921c828c4cdbdd3668
Aaron Meyer <a@ameyer.me>
2017-02-08 12:39:07 -0500

    Cut out pickle files.

--- recepmod/figures/FigureCommon.py
+++ recepmod/figures/FigureCommon.py
@@ -7,30 +7,9 @@ import matplotlib.lines as mlines
 import matplotlib.colors as mcolors
 from matplotlib import rc
 from matplotlib.font_manager import FontProperties
-#import h5py
-#from tqdm import tqdm
-##import seaborn as sns
-
-##plt.rc('text',usetex=True)
-
-def seaborn_colorblindGet():
-   # This function collects the collor palette settings used in seaborn-colorblind, so as
-   # to get all of seaborn's colors without the unwanted formatting effects of seaborn
-   # backColor is the background color used in seaborn's colorblind setting
-##   Colors = pd.read_pickle('seaborn_colorblindColors.pkl').values
-   Colors = pd.read_pickle('seaborn_colorblindColors2.pkl').values
-   backColor = tuple([Colors[j][0] for j in range(3)])
-   colors = []
-   for j in range(1,7):
-       colors.append(tuple([Colors[k][j] for k in range(3)]))
-   colors2 = []
-   for j in range(7,11):
-       colors2.append(tuple([Colors[k][j] for k in range(3)]))
-   return colors, backColor, colors2
-
-colors, backColor, colors2 = seaborn_colorblindGet()
 
 Igs = {'IgG1':'o', 'IgG2':'d', 'IgG3':'s', 'IgG4':'^'}
+colors = ['r', 'g', 'b', 'y', 'k', 'm']
 
 fcgrs = [r'Fc$\gamma$RI',r'Fc$\gamma$RIIA-131R',r'Fc$\gamma$RIIA-131H',r'Fc$\gamma$RIIB',r'Fc$\gamma$RIIIA-158F',r'Fc$\gamma$RIIIA-158V']
 FcgRs = {}
