6ec819808c80d6c89f668c558476b80e05170914
Aaron Meyer <a@ameyer.me>
2017-02-13 19:29:18 -0500

    Two receptor model completed (#36)
    * Added tests for Req method.
    * Class to handle variables.
    * Reorganized StoneTwo class to work.
    * Complete testing coverage of core methods.

--- recepmod/tests/test_StoneTwoRecep.py
+++ recepmod/tests/test_StoneTwoRecep.py
@@ -64,6 +64,31 @@ class TestStoneTwoRecpMethods(unittest.TestCase):
         # Check that detailed balance holds
         self.assertTrue(np.all(np.equal(output, np.transpose(output2))))
 
+    # Test that varying Req and Ka provides the expected results
+    def test_vGridVar(self):
+        Req = np.array([1E4, 1E3], dtype = np.float64)
+        Ka = np.array([1E3, 1E3], dtype = np.float64)
+        L0 = 1.000E-7
+        Kx = 1.0E-5
+        gnu = 4
+
+        output = StoneVgrid(Req,Ka,gnu,Kx,L0)
+        self.assertGreater(output[3,0], output[0,3])
+
+        Req = np.flipud(Req)
+        output = StoneVgrid(Req,Ka,gnu,Kx,L0)
+        self.assertLess(output[3,0], output[0,3])
+
+        Req[0:2] = [1E3, 1E3]
+        Ka[0:2] = [1E4, 1E3]
+
+        output = StoneVgrid(Req,Ka,gnu,Kx,L0)
+        self.assertGreater(output[3,0], output[0,3])
+
+        Ka = np.flipud(Ka)
+        output = StoneVgrid(Req,Ka,gnu,Kx,L0)
+        self.assertLess(output[3,0], output[0,3])
+
     def test_StoneRbnd(self):
         gnu = 4
 
@@ -82,6 +107,87 @@ class TestStoneTwoRecpMethods(unittest.TestCase):
         self.assertEqual(output[1], 20)
         self.assertEqual(outputmulti[0], 19)
         self.assertEqual(outputmulti[1], 19)
+
+        vGrid[2,0] = 2
+
+        output = StoneRbnd(vGrid)
+        outputmulti = StoneRmultiAll(vGrid)
+
+        self.assertEqual(output[0], 22)
+        self.assertEqual(output[1], 20)
+        self.assertEqual(outputmulti[0], 21)
+        self.assertEqual(outputmulti[1], 19)
+
+    # This processes checks that the relationships with Rbnd make sense when
+    # considering variation in Req and Ka
+    def test_Rbnd_vGrid(self):
+        logR = np.array([3, 2], dtype=np.float64)
+        Ka = np.array([1E4, 1E4], dtype=np.float64)
+        gnu = 10
+        Kx = 1E-5
+        L0 = 1E-5
+
+        Rbnd = StoneRbnd(StoneVgrid(np.power(10,logR),Ka,gnu,Kx,L0))
+
+        self.assertGreater(Rbnd[0], Rbnd[1])
+
+        logR = np.flipud(logR)
+
+        Rbnd = StoneRbnd(StoneVgrid(np.power(10,logR),Ka,gnu,Kx,L0))
+
+        self.assertGreater(Rbnd[1], Rbnd[0])
+
+
+        logR[0:2] = 2.0
+
+        Ka[0:2] = [1E4, 1E5]
+
+        Rbnd = StoneRbnd(StoneVgrid(np.power(10,logR),Ka,gnu,Kx,L0))
+
+        self.assertGreater(Rbnd[1], Rbnd[0])
+
+    def retReqDebug(self, logR, Ka):
+        gnu = 10
+        Kx = 1E-5
+        L0 = 1E-5
+
+        output = reqSolver(logR,Ka,gnu,Kx,L0)
+
+        vGrid = StoneVgrid(np.power(10,output),Ka,gnu,Kx,L0)
+
+        Rbnd = StoneRbnd(vGrid)
+
+        Rtot = np.power(10,output) + Rbnd
+
+        deltaR = np.linalg.norm(Rtot - np.power(10,logR))
+
+        self.assertAlmostEqual(deltaR, 0.0, delta = 0.1, msg = "Mass balance failed on reqSolver")
+
+        return (output, vGrid, Rbnd)
+
+    def test_reqSolver(self):
+        logR = np.array([5, 5], dtype=np.float64)
+        Ka = np.array([1E5, 1E4], dtype=np.float64)
+
+
+        output, vGrid, Rbnd = self.retReqDebug(logR, Ka)
+        self.assertGreater(output[1], output[0])
+
+        # Flip Ka around and see the relationship still applies
+        Ka[0:2] = [1.0E2, 1.0E6]
+        output, vGrid, Rbnd = self.retReqDebug(logR, Ka)
+        self.assertGreater(output[0], output[1])
+
+        # Now keep Ka equal but have asymmetric expression
+        Ka[0:2] = 1.0E5
+        logR[0:2] = [4.0, 5.0]
+        output, vGrid, Rbnd = self.retReqDebug(logR, Ka)
+        self.assertGreater(output[1], output[0])
+
+        # And perform same with expression switched
+        logR[0:2] = [5.0, 4.0]
+        output, vGrid, Rbnd = self.retReqDebug(logR, Ka)
+        self.assertGreater(output[0], output[1])
 
 if __name__ == '__main__':
     unittest.main()
