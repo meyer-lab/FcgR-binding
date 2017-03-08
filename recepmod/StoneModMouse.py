6a4027beab96608c159434fdf1f32565c8dfac78
nguan <ningguan@mit.edu>
2017-03-07 23:28:20 -0500

    Change input parameters, added RmultiAvidityTable with Rmulti greater than v in range(1,v)

--- recepmod/StoneModMouse.py
+++ recepmod/StoneModMouse.py
@@ -2,6 +2,7 @@ from .StoneModel import StoneMod
 import numpy as np
 import os
 import pandas as pd
+from memoize import memoize
 from scipy.optimize import brentq
 from scipy.misc import comb
 from sklearn import linear_model
@@ -39,6 +40,9 @@ class StoneModelMouse:
             if self.Igs[i] == x[self.IgIDX]:
                 x1[self.IgIDX] = np.nan
                 l = i
+#        if type(x[self.IgIDX]) != int:
+#            return (np.nan, np.nan, np.nan)
+        #print(x[self.IgIDX])
 
         # Assign inputs for StoneMod
         x1 = np.array(x1)
@@ -77,8 +81,9 @@ class StoneModelMouse:
         if fullOutput:
             return (outputLbnd, outputRbnd, outputRmulti, outputnXlink, outputReq)
         return (outputLbnd, outputRbnd, outputReq)
-
-    def pdOutputTable(self, x, fullOutput = False):
+        
+    def pdOutputTable(self, z, fullOutput = False):
+        # Takes in a list of shape (8) for z in the format of [logR, logR, logR, logR, logR, logR, kx, v, Li]
         # Organizes the binding prediction between the 24 Ig-FcgR pairs calculated by StoneModMouse(x)
         # Outputs a pandas table of binding prediction
         stoneModMurine = []
@@ -97,11 +102,13 @@ class StoneModelMouse:
         # Make a 3-d array of StoneModMouse output for each Ig
         if fullOutput:
             for i in range(len(self.Igs)):
-                x[self.IgIDX] = self.Igs[i]
+                x = z[:]
+                x.insert(self.IgIDX, self.Igs[i])
                 stoneModMurine.append(np.transpose(self.StoneModMouse(x, fullOutput = True)))
         else:
             for i in range(len(self.Igs)):
-                x[self.IgIDX] = self.Igs[i]
+                x = z[:]
+                x.insert(self.IgIDX, self.Igs[i])
                 stoneModMurine.append(np.transpose(self.StoneModMouse(x)))
 
         # Reshape data for pandas table
@@ -115,22 +122,28 @@ class StoneModelMouse:
         table = pd.DataFrame(np.array(output), index = self.Igs, columns = labels)
         return table
 
-    def pdAvidityTable(self, x, vl, vu, fullOutput = False):
-        # Takes in a list of shape (9) for x, lower bond for avidity vl, and upper bond for avidity vu
+    def pdAvidityTable(self, y, vl, vu, fullOutput = False):
+        # Takes in a list of shape (8) for y <x without avidity v>, lower bond for avidity vl, and upper bond for avidity vu
         # Organizes a pandas table of binding predictions for a given Ig as avidity varies
         tb1 = pd.DataFrame()
-        Ig = x[self.IgIDX]
+        Ig = y[self.IgIDX]
         idx = []
         # Concatenating a pandas table for a range of avidity
         if fullOutput == False:
             for i in range(vl, vu+1):
-                x[self.uvIDX] = i
-                tb = self.pdOutputTable(x, fullOutput = False)
+                x = y[:]
+                x.insert(self.uvIDX, i)
+                x.pop(self.IgIDX)
+                z = x
+                tb = self.pdOutputTable(z, fullOutput = False)
                 tb1 = pd.concat([tb1, tb.loc[[Ig]]])
         elif fullOutput == True:
             for j in range(vl, vu+1):
-                x[self.uvIDX] = j
-                tb = self.pdOutputTable(x, fullOutput = True)
+                x = y[:]
+                x.insert(self.uvIDX, j)
+                x.pop(self.IgIDX)
+                z = x
+                tb = self.pdOutputTable(z, fullOutput = True)
                 tb1 = pd.concat([tb1, tb.loc[[Ig]]])
         # Indexing
         for k in range(vl, vu+1):
@@ -138,18 +151,19 @@ class StoneModelMouse:
         tb1.index = idx
         return tb1
 
-    def NimmerjahnEffectTable(self, x):
+    def NimmerjahnEffectTable(self, z):
+        # Makes a pandas dataframe of shape(8,31) with 2 different avidities for each 1gG
         # Initiate variables
-        x1 = x[:]
+        z1 = z[:]
         tbN = pd.DataFrame()
         idx = []
         # create pandas tables
-        tv = self.pdOutputTable(x, fullOutput = True)
-        x1[self.uvIDX] = 1
-        t1 = self.pdOutputTable(x1, fullOutput = True)
+        tv = self.pdOutputTable(z, fullOutput = True)
+        z1[self.uvIDX-1] = 1
+        t1 = self.pdOutputTable(z1, fullOutput = True)
         # Compose a table of shape (8,30), 2 rows for each IgG
         for i in self.Igs:
-            for j in [1, x[self.uvIDX]]:
+            for j in [1, z[self.uvIDX-1]]:
                 if j == 1:
                     tbN = pd.concat([tbN, t1.loc[[i]]])
                 else:
@@ -159,11 +173,11 @@ class StoneModelMouse:
         # Append effectiveness data on the 31 column
         tbN.loc[:,'Effectiveness'] = pd.Series([0,0,0,0.95,0,0.20,0,0], index=tbN.index)
         return tbN
-
-    def NimmerjahnMultiLinear(self, x, fullOutput = True):
+    
+    def NimmerjahnMultiLinear(self, z, fullOutput = True):
         # Multi-Linear regression of FcgR binding predictions for effectiveness of IgG therapy
         reg = linear_model.LinearRegression()
-        tbN = self.NimmerjahnEffectTable(x)
+        tbN = self.NimmerjahnEffectTable(z)
         # Assign independent variables and dependent variable "effect"
         # Current independent variables: FcgRbnd for FcgRI, FcgRIIB, FcgRIII, and FcgRIV
         independent = np.array(tbN.iloc[list(range(2,6)), list(range(1,21,5))].apply(np.log10))
@@ -172,16 +186,20 @@ class StoneModelMouse:
         effect = effect.reshape(4,1)
         # Linear regression and plot result
         result = reg.fit(independent, effect)
-
+#        plt.scatter(effect, reg.predict(independent), color='black')
+#        plt.plot(effect, reg.predict(independent), color='blue', linewidth=3)
+        #plt.show()
         return result
 
-    def NimmerjahnLasso(self, x, fullOutput = True):
+    def NimmerjahnLasso(self, z, fullOutput = True):
         # Lasso regression of IgG1, IgG2a, and IgG2b effectiveness with binding predictions as potential parameters
         las = linear_model.Lasso(alpha = 0.005, normalize = True)
-        tbN = self.NimmerjahnEffectTable(x)
+        tbN = self.NimmerjahnEffectTable(z)
+        tbNparam = tbN.iloc[:, list(range(30))]
+        tbN_norm = (tbNparam - tbNparam.mean()) / (tbNparam.max() - tbNparam.min())
         # Assign independent variables and dependent variable "effect"
         # Current independent variables: FcgRbnd for FcgRI, FcgRIIB, FcgRIII, and FcgRIV
-        independent = np.array(tbN.iloc[list(range(6)), list(range(20))])
+        independent = np.array(tbN_norm.iloc[list(range(6)), list(range(20))])
         independent = independent.reshape(6,20)
         effect = np.array(tbN.iloc[list(range(6)),30])
         effect = effect.reshape(6,1)
@@ -195,12 +213,12 @@ class StoneModelMouse:
         plt.plot(effect, las.predict(independent), color='blue', linewidth=3)
         plt.show()
         return res
-
-    def FcgRPlots(self, x):
+    
+    def FcgRPlots(self, z):
         # Plot effectiveness vs. all FcgR binding parameters
-        tbN = self.NimmerjahnEffectTable(x)
+        tbN = self.NimmerjahnEffectTable(z)
         tbNparam = tbN.iloc[:, list(range(30))]
-        tbN_norm = (tbNparam - tbNparam.mean()) / (tbNparam.max() - tbNparam.min())
+        tbN_norm = (tbNparam - tbNparam.mean()) / (tbNparam.max()- tbNparam.min())
         # Initiate variables
         bndParam = []
         eff = []
@@ -221,3 +239,93 @@ class StoneModelMouse:
         plotTb = np.transpose(plotTb)
         table = pd.DataFrame(plotTb, columns = ['index', 'bndParam', 'eff'])
         sns.lmplot(x="bndParam", y="eff", col = 'index', hue = 'index', col_wrap=2, ci=None, palette="muted", data=table, size = 3)
+    
+    def Rmulti_v(self, kx, ka, L0, logR0, v):
+        # Returns the number of receptors bond at or above each avidity
+        # Initiate variables
+        R0 = np.power(10,logR0)
+        sigma = 0.001
+        Req = R0/2 
+        high = R0
+        low = 0
+        L_bound = 0
+        L = []
+        Rmultiv = []
+        while abs(R0 - Req*(1 + v*L0*ka*(1+kx*Req)**(v-1))) > sigma:
+            if (R0 - Req*(1 + v*L0*ka*(1+kx*Req)**(v-1))) > 0:
+                low = Req
+            elif (R0 - Req*(1 + v*L0*ka*(1+kx*Req)**(v-1))) < 0:
+                high = Req
+            Req = (high+low)/2
+        for i in range(1, int(v+1)):
+            L.append(comb(v,i)*(kx**(i-1))*L0*ka*(Req**i))
+            L_bound += L[i-1]
+        R = L[:]
+        for j in range(len(L)):
+            R[j] = R[j]*(j+1)
+        for k in range(len(R)):
+            Rmultiv.append(sum(R[k:len(R)]))
+        Rmultiv = np.array(Rmultiv)
+        return Rmultiv
+
+    def RmultiAvidity(self, x):
+        # Assign Ig type to a number corresponding to the row of Ka
+        x1 = x[:]
+        for i in range(4):
+            if self.Igs[i] == x[self.IgIDX]:
+                x1[self.IgIDX] = np.nan
+                l = i
+
+        # Assign inputs for Rmulti_v
+        x1 = np.array(x1)
+        v = x1[self.uvIDX]
+        Kx = np.power(10, x1[self.kxIDX])
+        L0 = x1[self.L0IDX]
+
+        # Initiate numpy arrays for StoneMod outputs
+        outputRmultiv = np.full((6,v), np.nan)
+
+        # Iterate over each FcgR
+        for k in range(6):
+            logR = x1[k]
+            ## Set the affinity for the binding of the FcgR and IgG in question
+            Ka = self.kaMouse[k][l]
+            if Ka == '+' or np.isnan(Ka):
+                continue
+            Ka = float(Ka)
+            ## Calculate the MFI which should result from this condition according to the model
+            RmultivOut = self.Rmulti_v(Kx, Ka, L0, logR, v)
+            outputRmultiv[k] = RmultivOut
+        outputRmultiv = outputRmultiv.reshape(6,int(v))
+        return outputRmultiv
+    
+    def RmultiAvidityTable(self,z):
+        # Organizes the binding prediction between the 24 Ig-FcgR pairs calculated by StoneModMouse(x)
+        # Outputs a pandas table of binding prediction
+        v = z[self.uvIDX-1]
+        Rmultiv = []
+        labels = []
+
+        # Set labels for columns of pandas table
+        for i in self.FcgRs:
+            for j in range(v):
+                labels.append(i+'-'+str(j+1))
+
+        # Make a 3-d array of StoneModMouse output for each Ig
+        for i in range(len(self.Igs)):
+            x = z[:]
+            x.insert(self.IgIDX, self.Igs[i])
+            Rmultiv.append(self.RmultiAvidity(x))
+
+        # Reshape data for pandas table
+        output = np.array(Rmultiv)
+        output = np.reshape(output,(4,6*v))
+
+        # Make pandas table of binding predictions of Ig-FcgR pairs
+        tbv = pd.DataFrame(np.array(output), index = self.Igs, columns = labels)
+        # Append effectiveness data on the last column
+        tbv.loc[:,'Effectiveness'] = pd.Series([0,0.95,0.20,0], index=tbv.index)
+        return tbv
+    
+#    def RmultiAvidityLasso(self, x):
+#        
