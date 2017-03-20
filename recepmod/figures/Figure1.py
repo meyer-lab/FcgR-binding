c2c20e24a4411778362517f8b7d279d6ae36940c
robbobbinett <rrobinet@mit.edu>
2017-03-20 01:10:25 -0400

    I relabelled and moved aroud the subfigures in Figure 1 in the manner requested. To do this, I added new parameters to many of the functions in Figure1.py to allow for manipulation of the positions of the yticklabels and ylabels.

--- recepmod/figures/Figure1.py
+++ recepmod/figures/Figure1.py
@@ -7,7 +7,7 @@ from ..StoneModel import StoneModel
 from ..StoneHelper import getFitMeasSummarized, getMeasuredDataFrame
 from .FigureCommon import Igs, FcgRidx, makeFcIgLegend, subplotLabel, FcgRidxL
 
-def plotNormalizedBindingvsKA(fitMean, ax1=None, ax2=None):
+def plotNormalizedBindingvsKA(fitMean, ax1=None, ax2=None, ylabelpad=-0.3, ytickx=0.08):
     # Select the subset of data we want
     fitMean = fitMean[['Ig', 'TNP', 'FcgR', 'Ka', 'Meas_mean', 'Meas_std', 'Expression_mean']]
 
@@ -35,7 +35,7 @@ def plotNormalizedBindingvsKA(fitMean, ax1=None, ax2=None):
 
         axInt.loglog()
         axInt.set_xlabel(r'Fc$\gamma$R-IgG Ka')
-        axInt.set_ylabel('Measured TNP-BSA binding')
+        axInt.set_ylabel('Measured TNP-BSA binding',labelpad=ylabelpad)
         axInt.set_ylim(1.0E-3, 1.0E-1)
 
     plotF(ax1, fitMean.loc[fitMean['TNP'] == "TNP-4",:])
@@ -44,7 +44,12 @@ def plotNormalizedBindingvsKA(fitMean, ax1=None, ax2=None):
     ax1.set_title('TNP-4-BSA')
     ax2.set_title('TNP-26-BSA')
 
-def plotAvidityEffectVsKA(fitMean, ax1=None):
+    for elem in ax1.get_yticklabels():
+        elem.set_x(ytickx)
+    for elem in ax2.get_yticklabels():
+        elem.set_x(ytickx)
+
+def plotAvidityEffectVsKA(fitMean, ax1=None, ylabelpad=-0.3, ytickx = 0.08):
     # Make axes if none exist
     if ax1 is None:
         ax1 = plt.figure().add_subplot(121)
@@ -79,13 +84,15 @@ def plotAvidityEffectVsKA(fitMean, ax1=None):
 
     ax1.loglog()
     ax1.set_xlabel(r'Fc$\gamma$R-IgG Ka')
-    ax1.set_ylabel('TNP-26 / TNP-4 Binding')
+    ax1.set_ylabel('TNP-26 / TNP-4 Binding',labelpad=ylabelpad)
+    for j in range(6):
+        ax1.get_yticklabels()[j].set_x(ytickx)
     ax1.set_ylim(1, 20)
 
     ax1.legend(handles=makeFcIgLegend(), bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 
-def FcgRQuantificationFigureMaker(StoneM, ax=None):
+def FcgRQuantificationFigureMaker(StoneM, ax=None, ylabelpad=0, ytickx=0):
     # Put receptor expression measurements into a dataframe
     df = pd.DataFrame(StoneM.Rquant).T
 
@@ -109,9 +116,11 @@ def FcgRQuantificationFigureMaker(StoneM, ax=None):
     ## Set up axes
     axx.set_yscale('log')
     axx.set_ylim(1.0E5, 1.0E7)
-    axx.set_ylabel("Receptors/Cell")
+    axx.set_ylabel("Receptors/Cell", labelpad=ylabelpad)
     ax.set_xlabel("")
     axx.set_xlabel("")
+    for elem in axx.get_yticklabels():
+        elem.set_x(ytickx)
     axx.set_xticklabels(axx.get_xticklabels(), rotation=40, rotation_mode="anchor", ha="right")
 
 def mfiAdjMeanFigureMaker(measAll, axarr=None):
@@ -146,7 +155,7 @@ def makeFigure():
     sns.set(style="whitegrid", font_scale=0.7, color_codes=True, palette="colorblind")
 
     f = plt.figure(figsize=(7,6))
-    gs1 = gridspec.GridSpec(3,6,height_ratios=[4,1,6],width_ratios=[16,4,20,4,20,0])
+    gs1 = gridspec.GridSpec(3,8,height_ratios=[4,1,6],width_ratios=[20,4,20,4,20,4,20,16])
     ax = f.add_subplot(gs1[0])
     FcgRQuantificationFigureMaker(StoneM,ax)
 
@@ -160,22 +169,23 @@ def makeFigure():
 
     plotNormalizedBindingvsKA(fitMean, ax2, ax3)
 
-    subplotLabel(ax2, 'C')
-    subplotLabel(ax3, 'D')
+    subplotLabel(ax2, 'B')
+    subplotLabel(ax3, 'C')
 
-    gs2 = gridspec.GridSpec(8,7,height_ratios=[4,4,4,6,8,12,7,12],width_ratios=[4,1,4,1,4,1,5])
+    ax4 = f.add_subplot(gs1[6])
+    plotAvidityEffectVsKA(fitMean,ax4)
+
+    subplotLabel(ax4, 'D')
+
+    gs2 = gridspec.GridSpec(8,5,height_ratios=[4,4,4,6,8,12,7,12],width_ratios=[4,1,4,1,4])
     axarr = []
     for j in range(6):
-        axarr.append(f.add_subplot(gs2[35+2*j+8*int(np.floor(j/3))]))
+        axarr.append(f.add_subplot(gs2[25+2*j+4*int(np.floor(j/3))]))
 
-    subplotLabel(axarr[0], 'B')
+    subplotLabel(axarr[0], 'E')
 
     mfiAdjMeanFigureMaker(measAll,axarr)
 
     gs3 = gridspec.GridSpec(4,3,height_ratios=[26,12,6,6],width_ratios=[15,2,5])
-    ax4 = f.add_subplot(gs3[5])
-    plotAvidityEffectVsKA(fitMean,ax4)
-
-    subplotLabel(ax4, 'E')
 
     return f
