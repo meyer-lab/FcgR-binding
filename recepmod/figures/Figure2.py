a5aaf59fdfd0622e6f511d57a2a6947024fd036b
Aaron Meyer <a@ameyer.me>
2017-03-24 13:48:00 -0400

    Function for model predicted quantities in orig. experiment. And removed ipynb files.

--- recepmod/figures/Figure2.py
+++ recepmod/figures/Figure2.py
@@ -165,3 +165,48 @@ def plotFit(fitMean, ax=None):
     ax.loglog()
     ax.set_ylim(0.01, 5)
     ax.set_xlim(0.01, 5)
+
+def plotFitBinding(M, dset, quant = "LbndPred", axarr = None):
+    from ..StoneHelper import mapMCMC, getFitPrediction, reduceMCMC
+
+    dset = dset.loc[dset['LL'] > np.min(dset['LL']) - 3,:]
+    dsetSamp = dset.sample(1000)
+
+    runFunc = lambda x: getFitPrediction(M, x[2:])
+
+    output = mapMCMC(runFunc, dsetSamp)
+
+    output[quant] = output.groupby(['pSetNum'])[quant].apply(lambda x: x / x.mean())
+
+    if axarr is None:
+        f = plt.figure()
+
+        # Make grid
+        gs1 = gridspec.GridSpec(2,3)
+
+        # Create 6 axes for each FcgR
+        axarr = [ f.add_subplot(gs1[x]) for x in range(6) ]
+
+        built = True
+    else:
+        built = False
+
+    fcIter = zip(axarr, FcgRidx.keys())
+
+    # Loop through receptors creating plot
+    for axx, fcr in fcIter:
+        sns.boxplot(x="Ig",
+                    y = quant,
+                    hue="TNP",
+                    data=output.loc[output['FcgR'] == fcr,:],
+                    ax = axx,
+                    showfliers=False)
+
+        axx.set_ylabel("Binding (RU)")
+        axx.set_xlabel("")
+        axx.set_ylim((0, np.nanmax(output.loc[output['FcgR'] == fcr,quant].as_matrix())*1.05))
+        axx.legend_.remove()
+        axx.set_title(fcr)
+
+    if built is True:
+        plt.tight_layout()
