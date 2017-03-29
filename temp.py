7ff472564a37f7077845e06b846176e4bc1bee8b
robbobbinett <rrobinet@mit.edu>
2017-03-28 21:05:51 -0400

    Negligible progress. Commit made to prevent loss of progress from switching branches.

--- temp.py
+++ temp.py
@@ -1,5 +1,11 @@
-from recepmod.StoneModel import StoneModel
-from recepmod.StoneHelper import *
+import numpy as np
+from pymc3 import Model, Normal
+from pymc3.diagnostics import geweke
+a = np.random.normal(size=(1,10000))
 
-M = StoneModel()
-print(monovalentL(M))
+mod = Model()
+
+with mod:
+    alpha = a
+
+b = geweke(pymc3_obj=mod,first=0.1,last=0.1,intervals=50)
