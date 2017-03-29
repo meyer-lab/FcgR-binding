c39ae16fb2712b40c89eb22b766dd8304861e150
robbobbinett <rrobinet@mit.edu>
2017-03-29 00:54:41 -0400

    Created functions in StoneHelper.py to calculate the Geweke Diagnostic t-test values and p-values for each chain in the MCMC. Have yet to do the multivariate Geweke which cross-examines all the chains.

--- temp.py
+++ temp.py
@@ -1,11 +1,27 @@
 import numpy as np
-from pymc3 import Model, Normal
-from pymc3.diagnostics import geweke
-a = np.random.normal(size=(1,10000))
+import recepmod.StoneHelper as sh
+from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
 
-mod = Model()
+a = np.random.rand(100000)
+b = np.random.rand(10000)
 
-with mod:
-    alpha = a
+# Retrieve model and fit from hdf5 file
+##M, dset = read_chain(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/test_chain.h5"))
+_, dset = sh.read_chain('recepmod/data/test_chain.h5')
 
-b = geweke(pymc3_obj=mod,first=0.1,last=0.1,intervals=50)
+##print(dset.drop(['LL','walker'],1).as_matrix())
+##dsett = dset.drop(['LL','walker'],1).as_matrix()
+##for j in range(dsett.shape[1]):
+##    temp = dsett[:,j]
+##    print(sh.geweke(M, dset = read_chain(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/test_chain.h5"))))
+
+print(sh.geweke_chain(dset))
+
+c, d = ttest_ind(a,b)
+
+for j in range(2,dset.shape[1]):
+    plt.figure()
+    ax = plt.gca()
+    ax.plot(dset.as_matrix()[:,j])
+    plt.show()
