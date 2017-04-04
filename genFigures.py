6d26414c62f78cc101846fc4a2747acfcb6e1dc0
Aaron Meyer <a@ameyer.me>
2017-04-04 09:47:29 -0400

    Lint root files.

--- genFigures.py
+++ genFigures.py
@@ -1,19 +1,13 @@
-from matplotlib.backends.backend_pdf import PdfPages
-import matplotlib.pyplot as plt
-from recepmod.figures import Figure1
-from recepmod.figures import Figure2
-from recepmod.figures import Figure3
-from recepmod.figures import Figure4
-from recepmod.figures import Figure5
+from recepmod import figures
 
 def runFunc(figClass, nameOut):
     ff = figClass.makeFigure()
     ff.savefig('./Manuscript/Figures/' + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
     ff.savefig('./Manuscript/Figures/' + nameOut + '.pdf', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
-runFunc(Figure1, 'Figure1')
+runFunc(figures.Figure1, 'Figure1')
 
-runFunc(Figure2, 'Figure2')
+runFunc(figures.Figure2, 'Figure2')
 
-runFunc(Figure3, 'Figure3')
+runFunc(figures.Figure3, 'Figure3')
 
-runFunc(Figure4, 'Figure4')
+runFunc(figures.Figure4, 'Figure4')
