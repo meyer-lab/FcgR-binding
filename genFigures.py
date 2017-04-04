da82f01fd11b837338919833e1d5cf319251328d
Aaron Meyer <a@ameyer.me>
2017-04-04 09:53:34 -0400

    Fix error.

--- genFigures.py
+++ genFigures.py
@@ -1,13 +1,13 @@
-from recepmod import figures
+import recepmod
 
 def runFunc(figClass, nameOut):
     ff = figClass.makeFigure()
     ff.savefig('./Manuscript/Figures/' + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
     ff.savefig('./Manuscript/Figures/' + nameOut + '.pdf', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
-runFunc(figures.Figure1, 'Figure1')
+runFunc(recepmod.figures.Figure1, 'Figure1')
 
-runFunc(figures.Figure2, 'Figure2')
+runFunc(recepmod.figures.Figure2, 'Figure2')
 
-runFunc(figures.Figure3, 'Figure3')
+runFunc(recepmod.figures.Figure3, 'Figure3')
 
-runFunc(figures.Figure4, 'Figure4')
+runFunc(recepmod.figures.Figure4, 'Figure4')
