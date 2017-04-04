8005537f9db95e30f5d5522e81c9dda9d42a604a
Aaron Meyer <a@ameyer.me>
2017-04-04 09:55:39 -0400

    Try again

--- genFigures.py
+++ genFigures.py
@@ -1,13 +1,13 @@
-import recepmod
+from recepmod.figures import Figure1, Figure2, Figure3, Figure4
 
 def runFunc(figClass, nameOut):
     ff = figClass.makeFigure()
     ff.savefig('./Manuscript/Figures/' + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
     ff.savefig('./Manuscript/Figures/' + nameOut + '.pdf', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
-runFunc(recepmod.figures.Figure1, 'Figure1')
+runFunc(Figure1, 'Figure1')
 
-runFunc(recepmod.figures.Figure2, 'Figure2')
+runFunc(Figure2, 'Figure2')
 
-runFunc(recepmod.figures.Figure3, 'Figure3')
+runFunc(Figure3, 'Figure3')
 
-runFunc(recepmod.figures.Figure4, 'Figure4')
+runFunc(Figure4, 'Figure4')
