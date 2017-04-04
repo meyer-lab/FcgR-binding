ca9ef70994ec5af820d76b6045d8e7aa46c3e873
Aaron Meyer <a@ameyer.me>
2017-04-04 10:34:17 -0400

    Fix typo.

--- recepmod/figures/Figure4.py
+++ recepmod/figures/Figure4.py
@@ -95,7 +95,6 @@ def PredictionVersusAvidity(ax, Kx):
     ax[1].set_xlabel(r'Multimerized Fc$\gamma$R')
     ax[2].set_xlabel(r'Fc$\gamma$R Nxlinks')
 
-
 def TwoRecep(Kx, ax = None):
     """
     E) Predicted multimerized receptor versus avidity for RI + RIIB
