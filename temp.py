bc7b894ce24075d2ee66c0e6117e9c1dee8b976a
robbobbinett <rrobinet@mit.edu>
2017-04-09 23:49:19 -0400

    Trivial progress; this commit is for local branch switching on my machine.

--- temp.py
+++ temp.py
@@ -1,6 +1,14 @@
 import recepmod.StoneHelper as sh
 import numpy as np
+import pandas as pd
 
-_, dset = sh.read_chain('recepmod/data/test_chain.h5')
+M, dset = sh.read_chain('recepmod/data/test_chain.h5')
 ##M, dset = read_chain(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/test_chain.h5"))
-print(dset.loc[1.0,:])
+
+##dsett = dsett.T.rename(index=[dsett.T[j].loc[['walker']] for j in dsett.T.columns],columns=dsett.T.columns)
+##dsett = dsett.T
+##
+##dsett.columns = [item % 52 for item in dsett.columns]
+S, P = sh.geweke_chains(dset)
+
+print
