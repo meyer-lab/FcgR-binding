3dd0e946c60fdfa21e01d18263ced59f71450741
Aaron Meyer <a@ameyer.me>
2017-04-15 19:03:06 -0400

    Setup for predicting Ortiz and switch to enets for predictions.
    * Setup files for investigating Ortiz et al
    * More ortiz setup
    * Update gitignore and package code.
    * Don’t output plots during testing.
    * Setup Ortiz calculations for response data.
    * Can’t check for plotting functions on load or it messes up backend selection.
    * Reorganize crossvalidation and add affnity-based prediction.

---
+++ recepmod/ValidationOrtiz.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+from .StoneTwoRecep import StoneTwo
+
+class Ortiz:
+    def __init__(self):
+        import os
+
+        ## Find path for csv files, on any machine wherein the repository recepnum1 exists.
+        path = os.path.dirname(os.path.abspath(__file__))
+        self.Igs = ['IgG1', 'IgG2', 'IgG3', 'IgG4']
+        self.FcgRs = ['FcgRI', 'FcgRIIA-Arg', 'FcgRIIA-His', 'FcgRIIB', 'FcgRIIIA-Phe', 'FcgRIIIA-Val']
+
+        ## Define the matrix of Ka values from Bruhns
+        self.kaBruhns = np.loadtxt(os.path.join(path,'./data/FcgR-Ka-Bruhns.csv'), delimiter=',')
+
+        ## The valency and names of the different species
+        self.valency = np.array([1, 2, 3, 3, 5, 5], dtype = np.int)
+        self.structs = ['Fc1', 'Fc2', 'Fc3Y', 'Fc3L', 'Fc5X', 'Fc5Y']
+
+        ## Read in the Fc responses
+        self.FcResponse = pd.read_csv(os.path.join(path,'./data/ortiz/Fig2DE-response.csv'), comment='#')
+
+    def predictResponse(self):
+        ''' Predict the response measured. '''
+
+        Ka = [self.kaBruhns[0][0], self.kaBruhns[0][0]] # The affinity of the relevant interaction
+        L0 = 1E-4 # This is known
+        logR = [2, 3]
+        Kx = 1E-9
+
+        TwoModel = StoneTwo(logR, Ka, Kx)
+
+        outt = []
+
+        for ii, item in enumerate(self.structs):
+            result = TwoModel.getAllProps(self.valency[ii], L0)
+            result.name = item
+
+            outt.append(result)
+
+        pTable = pd.concat(outt, axis=1).T
+
+
+
+
