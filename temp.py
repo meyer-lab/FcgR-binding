a6bedf588c7d49c02adb58305f4cf7233d7acc19
robbobbinett <rrobinet@mit.edu>
2017-04-20 01:15:16 -0400

    Finishing function for pvalue generation for individual walkers. Working on making the figure to show maximal pvalue. Does not yet consider burn-in.

--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
 import recepmod.StoneHelper as sh
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 M, dset = sh.read_chain('recepmod/data/test_chain.h5')
 ##M, dset = read_chain(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/test_chain.h5"))
@@ -11,4 +12,29 @@ M, dset = sh.read_chain('recepmod/data/test_chain.h5')
 ##dsett.columns = [item % 52 for item in dsett.columns]
 S, P = sh.geweke_chains(dset)
 
-print
+def temp(dset):
+    print('Pre-alteration:')
+    print(dset)
+    nwalkers = int(np.max(dset['walker']))+1
+    dsett = dset.T
+    print('Transpose:')
+    print(dsett)
+    dsett.columns = [item % nwalkers for item in dsett.columns]
+    for j in range(2):
+        print(dsett[j].T)
+
+##temp(dset)
+Statistics, Pvalues = sh.geweke_chains(dset)
+##print(len(Statistics))
+##print('\n')
+##print(len(Statistics[0]))
+nwalkers = len(Pvalues)
+axx = []
+for j in range(13):
+    axx.append(plt.subplot(5,3,j+1))
+    temp = []
+    for item in Pvalues:
+        temp.append(item[j])
+    temp = np.array(temp)
+    axx[j].hist(temp)
+plt.show()
