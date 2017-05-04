1228d9dcdea0f900930f5e0893d8a10396216b1b
Aaron Meyer <a@ameyer.me>
2017-05-04 16:48:27 -0400

    New Figure 5.

--- recepmod/figures/Figure5.py
+++ recepmod/figures/Figure5.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib
 matplotlib.use('AGG')
 import numpy as np
@@ -5,22 +6,15 @@ import pandas as pd
 import seaborn as sns
 
 # Compare across species
+path = os.path.dirname(os.path.abspath(__file__))
 
 def makeFigure():
     import string
-    from itertools import product
     from matplotlib import gridspec
     import matplotlib.pyplot as plt
-    from .FigureCommon import subplotLabel, makeFcIgLegend
+    from .FigureCommon import subplotLabel
 
     sns.set(style="whitegrid", font_scale=0.7, color_codes=True, palette="colorblind")
-    pd.set_option('expand_frame_repr', False)
-
-    # Setup the table of conditions we'll use.
-    avidity = np.logspace(0, 3, 4, base=2, dtype=np.int)
-    ligand = np.logspace(start=-12, stop=-5, num=3)
-    IgID = np.arange(0, 4, dtype=np.int)
-    conditions = pd.DataFrame(list(product(avidity, ligand, IgID)), columns=['avidity', 'ligand', 'IgID'])
 
     # Setup plotting space
     f = plt.figure(figsize=(7, 5))
@@ -31,9 +25,15 @@ def makeFigure():
     # Get list of axis objects
     ax = [f.add_subplot(gs1[2*x]) for x in range(4)]
 
-    PCAmurine(ax[0:2], conditions)
+    # Run the murine plots
+    PCAplot(ax[0:2],
+            pd.read_csv(os.path.join(path, '../data/pca-murine.csv'), index_col=0),
+            'Murine')
 
-    PCAhuman(ax[2:4], conditions)
+    # Run the human plots
+    PCAplot(ax[2:4],
+            pd.read_csv(os.path.join(path, '../data/pca-human.csv'), index_col=0),
+            'Human')
 
     subplotLabel(ax[0], 'A')
     subplotLabel(ax[2], 'B')
@@ -47,46 +47,26 @@ def makeFigure():
 
     return f
 
-def calcActivity(affinities, expressions, conditions, activities):
-    """ Calculate the ligand bound and activity response for different cell populations. """
-    from ..StoneHelper import getMedianKx
-    from ..StoneNRecep import StoneN
-    from ..StoneModel import StoneMod
-
-    def applyF(condR):
-        for exprN, expr in expressions.items():
-            # Isolate receptors expressed, and keep the index of those
-            exprV = np.array(expr, dtype=np.float)
-            exprIDX = np.logical_not(np.isnan(exprV))
-            exprV = exprV[exprIDX]
-
-            # Pull out the relevant affinities from the table
-            affyH = affinities[exprIDX, int(condR.IgID)] + 0.1
-
-            try:
-                if exprV.size > 1:
-                    # Setup the StoneN model
-                    M = StoneN(logR=exprV, Ka=affyH, Kx=getMedianKx(), gnu=condR.avidity, L0=condR.ligand)
-
-                    condR[exprN + '_activity'] = M.getActivity(activities[exprN])
-                    condR[exprN + '_Lbnd'] = M.getLbnd()
-                else:
-                    output = StoneMod(exprV, affyH, condR.avidity, getMedianKx(), condR.ligand, fullOutput=True)
-
-                    condR[exprN + '_activity'] = output[3]
-                    condR[exprN + '_Lbnd'] = output[0]
-            except:
-                print(condR)
-                print(affyH)
-                print(exprV)
-                raise
-
-        return condR
-
-
-    return conditions.apply(applyF, axis=1)
-
 Igs = {0:'o', 1:'d', 2:'^', 3:'s'}
+mIgs = {'IgG1':'o', 'IgG2a':'d', 'IgG2b':'^', 'IgG3':'s'}
+hIgs = {'IgG1':'o', 'IgG2':'d', 'IgG3':'^', 'IgG4':'s'}
+quantShape = {'Lbnd':'o', 'activity':'d'}
+
+
+def Legend(ax, colors, shapes):
+    """ Make legend. """
+    import matplotlib.lines as mlines
+    import matplotlib.patches as mpatches
+
+    patches = list()
+
+    for key, val in colors.items():
+        patches.append(mpatches.Patch(color=val, label=key))
+
+    for key, val in shapes.items():
+        patches.append(mlines.Line2D([], [], color='black', marker=val, markersize=7, label=key, linestyle='None'))
+    
+    ax.legend(handles=patches, bbox_to_anchor=(1, 1), loc=2)
 
 
 def PCAplot(axes, dataIn, species):
@@ -99,8 +79,13 @@ def PCAplot(axes, dataIn, species):
 
     X = dataIn.drop(['avidity', 'ligand', 'IgID'], axis=1)
 
-    X = StandardScaler().fit_transform(X)
-    X = pca.fit_transform(X)
+    terms = X.columns
+
+    X = pca.fit_transform(StandardScaler().fit_transform(X))
+
+    print(pca.explained_variance_)
+
+    avcolors = dict(zip(dataIn['avidity'].unique(), sns.color_palette()))
 
     # Move PCs into dataframe
     for ii in range(4):
@@ -108,36 +93,27 @@ def PCAplot(axes, dataIn, species):
 
     for _, row in dataIn.iterrows():
         markerr=Igs[row['IgID']]
-        axes[0].errorbar(x=row['PC1'], y=row['PC2'], marker=markerr, ms=3.5)
+        avc = avcolors[row['avidity']]
+        axes[0].errorbar(x=row['PC1'], y=row['PC2'], marker=markerr, mfc=avc, ms=3.5)
 
-    loadings = pd.DataFrame(pca.components_, columns=['PC1', 'PC2', 'PC3', 'PC4'])
+    loadings = pd.DataFrame(pca.components_.T, columns=['PC1', 'PC2', 'PC3', 'PC4'])
+    loadings['terms'] = terms
+    loadings['cells'], loadings['quantity'] = loadings['terms'].str.split('_', 1).str
 
-    loadings.plot(x='PC1', y='PC2', ax=axes[1], kind='scatter')
+    colors = dict(zip(loadings['cells'].unique(), sns.color_palette()))
+
+    for _, row in loadings.iterrows():
+        markerr=quantShape[row['quantity']]
+        colorr = colors[row['cells']]
+        axes[1].errorbar(x=row['PC1'], y=row['PC2'], marker=markerr, mfc=colorr, ms=5)
 
     axes[0].set_title(species + ' Scores')
     axes[1].set_title(species + ' Loadings')
 
+    # Ok, now start on legend
+    Legend(axes[1], colors, quantShape)
 
-def PCAmurine(axes, conditions):
-    """ Principle Components Analysis of murine FcgR binding predictions """
-    from ..StoneModMouse import StoneModelMouse
-
-    affinities = StoneModelMouse().kaMouse
-    expressions = {'NK-like':[float('nan'), float('nan'), 4.0, float('nan')], 'DC-like':[3.0, 4.0, 3.0, 3.0]}
-    activities = {'DC-like':[1.0, -1.0, 1.0, 1.0]}
-
-    outt = calcActivity(affinities, expressions, conditions, activities)
-
-    PCAplot(axes, outt, 'Murine')
-
-
-def PCAhuman(axes, conditions):
-    from ..StoneModel import StoneModel
-
-    expressions = {'NK-Phe':[float('nan'), float('nan'), float('nan'), float('nan'), 4.0, float('nan')],
-                   'NK-Val':[float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), 4.0]}
-    activities = {}
-
-    outt = calcActivity(StoneModel().kaBruhns, expressions, conditions, activities)
-
-    PCAplot(axes, outt, 'Human')
+    if species == 'Human':
+        Legend(axes[0], avcolors, hIgs)
+    else:
+        Legend(axes[0], avcolors, mIgs)
