cba0c8f1e6171f0f5ba0ee89f861c6a10df091ef
Aaron Meyer <a@ameyer.me>
2017-05-06 20:53:32 -0400

    A test

--- recepmod/tests/test_StoneHelper.py
+++ recepmod/tests/test_StoneHelper.py
@@ -14,6 +14,37 @@ class TestStoneMethods(unittest.TestCase):
 
     	self.assertIsInstance(outt, numpy.float64)
 
+    def test_rep(self):
+    	from ..StoneHelper import rep
+
+    	outt = rep([2, 3], 3)
+
+    	self.assertEquals(len(outt), 6)
+    	self.assertEquals(numpy.sum(outt), 15)
+
+    	outt = rep([2, 3, 4], 4)
+
+    	self.assertEquals(len(outt), 12)
+    	self.assertEquals(numpy.sum(outt), 36)
+
+    def test_geweke(self):
+    	from ..StoneHelper import geweke
+
+    	input = numpy.random.normal(size=4000)
+    	inputTwo = numpy.random.normal(size=4000)
+
+    	a, b = geweke(input)
+
+    	self.assertGreater(b, 0.01)
+
+    	a, b = geweke(input, inputTwo)
+
+    	self.assertGreater(b, 0.01)
+
+    	a, b = geweke(input, inputTwo + 2)
+
+    	self.assertLess(b, 0.01)
+
 
 if __name__ == '__main__':
     unittest.main()
