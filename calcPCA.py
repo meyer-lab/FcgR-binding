a80246f9f54e078c013e64ac7ed1057fdd7f7d4d
robbobbinett <rrobinet@mit.edu>
2017-06-05 02:58:20 -0400

    modified .gitignore to allow for only temp.py (among the tempX.py scripts) to be tracked. Also, created various scripts to help in the execution of the combined PCA plot. This is not yet complete.

--- calcPCA.py
+++ calcPCA.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from recepmod.PCAanalysis import recalcPCA
+##from recepmod.PCAanalysis import recalcPCA
+from recepmod.temp import recalcPCA
 
 if __name__ == '__main__':
     recalcPCA()
--- recepmod/temp3.py
+++ recepmod/temp3.py
@@ -6,3 +6,12 @@ for cline in cell:
             print('expressions["'+cline+'-'+gen+'"] = [1.0, 1.0, 1.0, 1.0]')
         else:
             print('expressions["'+cline+'-'+gen+'"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]')
+
+print('\n')
+
+for cline in cell:
+    for gen in genotype:
+        if gen == 'murine':
+            print('activities["'+cline+'-'+gen+'"] = [1.0, -1.0, 1.0, 1.0]')
+        else:
+            print('activities["'+cline+'-'+gen+'"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]')
