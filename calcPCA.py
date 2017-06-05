7fed37417dfb3995479740142c54f59b2d6dab2a
robbobbinett <rrobinet@mit.edu>
2017-06-05 15:32:54 -0400

    reset calcPCA.py to what it is in master; successfully running PCA to form .csv

--- calcPCA.py
+++ calcPCA.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-##from recepmod.PCAanalysis import recalcPCA
-from recepmod.temp import recalcPCA
+from recepmod.PCAanalysis import recalcPCA
 
 if __name__ == '__main__':
     recalcPCA()
--- recepmod/data/cellExprAndAct.py
+++ recepmod/data/cellExprAndAct.py
@@ -1,35 +1,34 @@
 cell = ['NK','MO','DC']
 ##genotype = ['murine','human-Phe','human-Val']
-species = ['murine','human']
 genotype = ['Phe','Val']
 
 expressions = {}
 activities = {}
 
-# Organized first by cell line, then by genotype
-expressions["NK-murine-Phe"] = [1.0, 1.0, 1.0, 1.0]
-expressions["NK-murine-Val"] = [1.0, 1.0, 1.0, 1.0]
-expressions["NK-human-Phe"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-expressions["NK-human-Val"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-expressions["MO-murine-Phe"] = [1.0, 1.0, 1.0, 1.0]
-expressions["MO-murine-Val"] = [1.0, 1.0, 1.0, 1.0]
-expressions["MO-human-Phe"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-expressions["MO-human-Val"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-expressions["DC-murine-Phe"] = [1.0, 1.0, 1.0, 1.0]
-expressions["DC-murine-Val"] = [1.0, 1.0, 1.0, 1.0]
-expressions["DC-human-Phe"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-expressions["DC-human-Val"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+# Organized first by cell line, then by genotype.
+# The first list is for murine, the second for human.
+expressions["NK-Phe"] = [[1.0, 1.0, 1.0, 1.0],
+                         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+expressions["NK-Val"] = [[1.0, 1.0, 1.0, 1.0],
+                         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+expressions["MO-Phe"] = [[1.0, 1.0, 1.0, 1.0],
+                         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+expressions["MO-Val"] = [[1.0, 1.0, 1.0, 1.0],
+                         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+expressions["DC-Phe"] = [[1.0, 1.0, 1.0, 1.0],
+                         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+expressions["DC-Val"] = [[1.0, 1.0, 1.0, 1.0],
+                         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
 
-
-activities["NK-murine-Phe"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["NK-murine-Val"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["NK-human-Phe"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["NK-human-Val"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["MO-murine-Phe"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["MO-murine-Val"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["MO-human-Phe"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["MO-human-Val"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["DC-murine-Phe"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["DC-murine-Val"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["DC-human-Phe"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-activities["DC-human-Val"] = [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+activities["NK-Phe"] = [[1.0, -1.0, 1.0, 1.0],
+                        [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["NK-Val"] = [[1.0, -1.0, 1.0, 1.0],
+                        [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["MO-Phe"] = [[1.0, -1.0, 1.0, 1.0],
+                        [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["MO-Val"] = [[1.0, -1.0, 1.0, 1.0],
+                        [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["DC-Phe"] = [[1.0, -1.0, 1.0, 1.0],
+                        [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["DC-Val"] = [[1.0, -1.0, 1.0, 1.0],
+                        [1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
