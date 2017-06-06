880922ee30380f498d1482ac72e411f1da84e493
robbobbinett <rrobinet@mit.edu>
2017-06-06 11:15:06 -0400

    still working on fixing PCA csv generator; this commit is for the sake of switching to master.

--- recepmod/data/cellExprAndActGen.py
+++ recepmod/data/cellExprAndActGen.py
@@ -2,8 +2,10 @@ from cellExprAndAct import cell, genotype
 
 for cline in cell:
     for gen in genotype:
-        print('expressions["'+cline+'-'+gen+'"] = [[1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]')
+        print('expressions["'+cline+'-'+gen+'"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]')
+
+print('\n')
 
 for cline in cell:
     for gen in genotype:
-        print('activities["'+cline+'-'+gen+'"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]')
+        print('activities["'+cline+'-'+gen+'"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]')
