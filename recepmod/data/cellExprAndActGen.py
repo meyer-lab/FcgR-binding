97880a119d542fbdc63dcf7f72f0cf41cead6c81
robbobbinett <rrobinet@mit.edu>
2017-06-06 14:40:11 -0400

    Fixed the conditions dictionary to reflect that we are treating the two human genotypes as distinct species, along with the mouse species

--- recepmod/data/cellExprAndActGen.py
+++ recepmod/data/cellExprAndActGen.py
@@ -1,11 +1,9 @@
 from cellExprAndAct import cell, species
 
 for cline in cell:
-    for spec in species:
-        print('expressions["'+cline+'-'+spec+'"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]')
+    print('expressions["'+cline+'"] = [[nan, 3.0, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, 3.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]')
 
 print('\n')
 
 for cline in cell:
-    for spec in species:
-        print('activities["'+cline+'-'+spec+'"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]')
+    print('activities["'+cline+'"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]')
