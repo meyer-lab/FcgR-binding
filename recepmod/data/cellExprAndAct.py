411d7a0b5205ff6788c1328e165b27aa5a18a9a0
robbobbinett <rrobinet@mit.edu>
2017-06-06 14:40:11 -0400

    Fixed the conditions dictionary to reflect that we are treating the two human genotypes as distinct species, along with the mouse species

--- recepmod/data/cellExprAndAct.py
+++ recepmod/data/cellExprAndAct.py
@@ -8,23 +8,11 @@ activities = {}
 
 # Organized first by cell line, then by genotype.
 # The first list is for murine, the second for human.
-expressions["NK-murine"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["NK-human-Phe"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["NK-human-Val"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["MO-murine"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["MO-human-Phe"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["MO-human-Val"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["DC-murine"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["DC-human-Phe"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
-expressions["DC-human-Val"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["NK"] = [[nan, 3.0, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, 3.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["MO"] = [[nan, 3.0, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, 3.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["DC"] = [[nan, 3.0, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, 3.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
 
 
-activities["NK-murine"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["NK-human-Phe"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["NK-human-Val"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["MO-murine"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["MO-human-Phe"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["MO-human-Val"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["DC-murine"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["DC-human-Phe"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
-activities["DC-human-Val"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["NK"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["MO"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["DC"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
