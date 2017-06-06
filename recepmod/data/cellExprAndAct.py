ee5e5acd98f386b7adcdef47fe6cc02e5eff5d29
robbobbinett <rrobinet@mit.edu>
2017-06-06 14:00:37 -0400

    Changed how different human genotypes are treated; mouse, human-Phe, and human-Val are treated as different species. pca.csv generates, but still fixing bugs in temp.py. Found something interesting concerning PCAanalysis.py; commit made for the sake of switching to master.

--- recepmod/data/cellExprAndAct.py
+++ recepmod/data/cellExprAndAct.py
@@ -1,25 +1,30 @@
 from numpy import nan
 
 cell = ['NK','MO','DC']
-##genotype = ['murine','human-Phe','human-Val']
-genotype = ['Phe','Val']
+species = ['murine','human-Phe','human-Val']
 
 expressions = {}
 activities = {}
 
 # Organized first by cell line, then by genotype.
 # The first list is for murine, the second for human.
-expressions["NK-Phe"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-expressions["NK-Val"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-expressions["MO-Phe"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-expressions["MO-Val"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-expressions["DC-Phe"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-expressions["DC-Val"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+expressions["NK-murine"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["NK-human-Phe"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["NK-human-Val"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["MO-murine"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["MO-human-Phe"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["MO-human-Val"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["DC-murine"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["DC-human-Phe"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
+expressions["DC-human-Val"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]
 
 
-activities["NK-Phe"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]
-activities["NK-Val"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]
-activities["MO-Phe"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]
-activities["MO-Val"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]
-activities["DC-Phe"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]
-activities["DC-Val"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]
+activities["NK-murine"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["NK-human-Phe"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["NK-human-Val"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["MO-murine"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["MO-human-Phe"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["MO-human-Val"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["DC-murine"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["DC-human-Phe"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
+activities["DC-human-Val"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]
--- recepmod/data/cellExprAndActGen.py
+++ recepmod/data/cellExprAndActGen.py
@@ -1,11 +1,11 @@
-from cellExprAndAct import cell, genotype
+from cellExprAndAct import cell, species
 
 for cline in cell:
-    for gen in genotype:
-        print('expressions["'+cline+'-'+gen+'"] = [[1.0, 1.0, 0.0, 0.0],[1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]')
+    for spec in species:
+        print('expressions["'+cline+'-'+spec+'"] = [[nan, nan, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]')
 
 print('\n')
 
 for cline in cell:
-    for gen in genotype:
-        print('activities["'+cline+'-'+gen+'"] = [[1.0, -1.0, nan, nan],[1.0, 1.0, 1.0, nan, nan, nan, nan, nan]]')
+    for spec in species:
+        print('activities["'+cline+'-'+spec+'"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]')
