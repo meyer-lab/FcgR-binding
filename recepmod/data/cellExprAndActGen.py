7844006f12716d00e50a60563ec1be1871a3f010
robbobbinett <rrobinet@mit.edu>
2017-06-06 17:12:46 -0400

    PCA with all three "species" working, as well as naive PCA plot in Figure 5 for these. Now going to experiment with separate PCAs for each human genotype.

--- recepmod/data/cellExprAndActGen.py
+++ recepmod/data/cellExprAndActGen.py
@@ -1,9 +1,9 @@
 from cellExprAndAct import cell, species
 
 for cline in cell:
-    print('expressions["'+cline+'"] = [[nan, 3.0, 4.0, nan],[nan, nan, nan, nan, nan, 4.0, 3.0, nan],[nan, nan, nan, nan, nan, 4.0, nan, nan]]')
+    print('expressions["'+cline+'"] = [[3.0, 4.0, 3.0, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0]]')
 
 print('\n')
 
 for cline in cell:
-    print('activities["'+cline+'"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0],[1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0]]')
+    print('activities["'+cline+'"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, -1.0, 1.0, 1.0, 0.0],[1.0, 1.0, -1.0, 1.0, 1.0, 0.0]]')
