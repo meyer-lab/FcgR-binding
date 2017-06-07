18c92e5ef2129fad0cb37f6c2555a88123c6c04c
robbobbinett <rrobinet@mit.edu>
2017-06-07 11:46:13 -0400

    PCA now working for both human genotypes; unfortunately, I broke figure 5. Still working on that

--- recepmod/data/cellExprAndAct.py
+++ recepmod/data/cellExprAndAct.py
@@ -1,17 +1,16 @@
 from numpy import nan
 
 cell = ['NK','MO','DC']
-species = ['murine','human-Phe','human-Val']
+geno = ['human-Phe','human-Val]
 
-expressions = {}
-activities = {}
+expressions = {'human-Phe':{}}
+activities = {'human-Val':{}}
 
 # Organized first by cell line, then by genotype.
 # The first list is for murine, the second for human.
 expressions["NK"] = [[3.0, 4.0, 3.0, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0]]
 expressions["MO"] = [[3.0, 4.0, 3.0, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0]]
 expressions["DC"] = [[3.0, 4.0, 3.0, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0],[3.0, 3.0, nan, 3.0, nan, 4.0, 3.0, nan, 3.0]]
-
 
 activities["NK"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, -1.0, 1.0, 1.0, 0.0],[1.0, 1.0, -1.0, 1.0, 1.0, 0.0]]
 activities["MO"] = [[1.0, -1.0, 1.0, 1.0],[1.0, 1.0, -1.0, 1.0, 1.0, 0.0],[1.0, 1.0, -1.0, 1.0, 1.0, 0.0]]
