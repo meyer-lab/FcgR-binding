e291407a3db43e8a0979951637a32b1e2aab0f3a
robbobbinett <rrobinet@mit.edu>
2017-06-07 11:46:13 -0400

    PCA now working for both human genotypes; unfortunately, I broke figure 5. Still working on that

--- recepmod/figures/Figure5temp.py
+++ recepmod/figures/Figure5temp.py
@@ -11,23 +11,16 @@ def makeFigure(pcOne='PC 1', pcTwo='PC 2'):
     # Get list of axis objects
     ax, f = getSetup((7, 5), (2, 2))
 
-##    # Run the murine plots
-##    PCAplot(ax[0:2],
-##            pd.read_csv(os.path.join(path, '../data/pca-murine.csv'), index_col=0),
-##            'Murine', pcOne, pcTwo)
-##
-##    # Run the human plots
-##    PCAplot(ax[2:4],
-##            pd.read_csv(os.path.join(path, '../data/pca-human.csv'), index_col=0),
-##            'Human', pcOne, pcTwo)
-
-    # Run the plot
+    # Run the plots
     PCAplot(ax[0:2],
-            pd.read_csv(os.path.join(path, '../data/pca.csv'), index_col=0),
-            'Murine', pcOne, pcTwo)
-
+            pd.read_csv(os.path.join(path, '../data/pca-human-Phe.csv'), index_col=0),
+            'Human-Phe', pcOne, pcTwo)
+    PCAplot(ax[2:4],
+            pd.read_csv(os.path.join(path, '../data/pca-human-Val.csv'), index_col=0),
+            'Human-Val', pcOne, pcTwo)
 
     subplotLabel(ax[0], 'A')
+    subplotLabel(ax[2], 'B')
 
     # Tweak layout
     f.tight_layout(w_pad=12)
@@ -36,18 +29,20 @@ def makeFigure(pcOne='PC 1', pcTwo='PC 2'):
 
 
 def makeSupp(ax):
+    x = [j for j in range(10000)]
+    ax.plot(x,x)
 
-    path = os.path.dirname(os.path.abspath(__file__))
-
-    # Run the murine plots
-    PCAplot(ax[0:2],
-            pd.read_csv(os.path.join(path, '../data/pca-murine.csv'), index_col=0),
-            'Murine', 'PC 1', 'PC 2')
-
-    # Run the human plots
-    PCAplot(ax[2:4],
-            pd.read_csv(os.path.join(path, '../data/pca-human.csv'), index_col=0),
-            'Human', 'PC 1', 'PC 2')
+##    path = os.path.dirname(os.path.abspath(__file__))
+##
+##    # Run the murine plots
+##    PCAplot(ax[0:2],
+##            pd.read_csv(os.path.join(path, '../data/pca-murine.csv'), index_col=0),
+##            'Murine', 'PC 1', 'PC 2')
+##
+##    # Run the human plots
+##    PCAplot(ax[2:4],
+##            pd.read_csv(os.path.join(path, '../data/pca-human.csv'), index_col=0),
+##            'Human', 'PC 1', 'PC 2')
 
 
 def PCAplot(axes, dataIn, species, pcOne='PC 2', pcTwo='PC 3'):
@@ -58,13 +53,13 @@ def PCAplot(axes, dataIn, species, pcOne='PC 2', pcTwo='PC 3'):
     from .FigureCommon import Legend
 
     colors = dict(zip(range(5), sns.color_palette()))
-    Igs = {0:'o', 1:'d', 2:'^', 3:'s', 4:'v', 5:'<', 6:'>', 7:'1', 8:'2', 9:'3', 10: '4', 11:'h'}
-##    mIgList = ['IgG1', 'IgG2a', 'IgG2b', 'IgG3']
-##    mIgs = {'IgG1':'o', 'IgG2a':'d', 'IgG2b':'^', 'IgG3':'s'}
-##    hIgList = ['IgG1', 'IgG2', 'IgG3', 'IgG4']
-##    hIgs = {'IgG1':'o', 'IgG2':'d', 'IgG3':'^', 'IgG4':'s'}
+    Igs = {0:'o', 1:'d', 2:'^', 3:'s', 4:'o', 5:'d', 6:'^', 7:'s'}
+    fillstyles = {0:'full', 1:'full', 2:'full', 3:'full', 4:'none', 5:'none', 6:'none', 7:'none'}
     quantList = ['Lbnd','activity']
     quantShape = {'Lbnd':'o', 'activity':'d'}
+
+    IgList = ['mIgG1/hIgG1','mIgG2a/hIgG2','mIgG2b/hIgG3','mIgG3/hIgG4']
+    Igs = {'mIgG1/hIgG1':'o', 'mIgG2a/hIgG2':'d', 'mIgG2b/hIgG3':'^', 'mIgG3/hIgG4':'s'}
 
     pca = PCA(n_components=4)
 
@@ -83,14 +78,17 @@ def PCAplot(axes, dataIn, species, pcOne='PC 2', pcTwo='PC 3'):
         dataIn['PC ' + str(ii+1)] = X[:, ii]
 
     for _, row in dataIn.iterrows():
-        markerr=Igs[row['IgID']]
-        avc = avcolors[row['avidity']]
-        axes[0].errorbar(x=row[pcOne], y=row[pcTwo], marker=markerr, mfc=avc, ms=5)
+        markerr=Igs[int(row['IgID'])]
+        avc = 'none' if (row['IgID'] > 3) else avcolors[int(row['avidity'])]
+        mec = avcolors[int(row['avidity'])]
+        axes[0].errorbar(x=row[pcOne], y=row[pcTwo],
+                     markeredgewidth=1, marker=markerr,
+                     markeredgecolor=mec, markersize=7,
+                     markerfacecolor=avc, ms=5)
 
     loadings = pd.DataFrame(pca.components_.T, columns=['PC 1', 'PC 2', 'PC 3', 'PC 4'])
     loadings['terms'] = terms
     loadings['cells'], loadings['quantity'] = loadings['terms'].str.split('_', 1).str
-##    loadings['cellType'], loadings['cellGeno'] = loadings['cells'].str.split('-', 1).str
 
     colors = dict(zip(loadings['terms'].unique(), sns.color_palette()))
 
@@ -102,10 +100,11 @@ def PCAplot(axes, dataIn, species, pcOne='PC 2', pcTwo='PC 3'):
     axes[0].set_title(species + ' Scores')
     axes[1].set_title(species + ' Loadings')
 
-##    # Ok, now start on legend
+    # Ok, now start on legend
 ##    axes[1].legend(handles=Legend(loadings['cellType'].unique(), colors, quantList, quantShape), bbox_to_anchor=(1, 1), loc=2)
 ##
-##    if species == 'Human':
+##    axes[0].legend(handles=Legend(dataIn['avidity'].unique(), avcolors, IgList, Igs), bbox_to_anchor=(1, 1), loc=2)
+##    if species == 'Human-Phe':
 ##        axes[0].legend(handles=Legend(dataIn['avidity'].unique(), avcolors, hIgList, hIgs), bbox_to_anchor=(1, 1), loc=2)
 ##    else:
 ##        axes[0].legend(handles=Legend(dataIn['avidity'].unique(), avcolors, mIgList, mIgs), bbox_to_anchor=(1, 1), loc=2)
@@ -117,3 +116,4 @@ def PCAplot(axes, dataIn, species, pcOne='PC 2', pcTwo='PC 3'):
         axes[ii].set_xlim(-xlim*1.1, xlim*1.1)
         axes[ii].set_xlabel(pcOne)
         axes[ii].set_ylabel(pcTwo)
+    
