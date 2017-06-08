c89657689d889ddcbaa17eaa959bc1f24c7261db
Aaron Meyer <a@ameyer.me>
2017-06-08 19:21:59 -0400

    Consolidate figure generation.

--- genFigures.py
+++ genFigures.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import matplotlib
 matplotlib.use('AGG')
 import svgutils.transform as st
@@ -22,55 +23,65 @@ def runFunc(nameOut):
         exec('from recepmod.figures import ' + nameOut)
         ff = eval(nameOut + '.makeFigure()')
         ff.savefig(fdir + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
-        print(nameOut)
+        print(nameOut + ' is done.')
+        return None
     except Exception as e:
         return e
 
 
 if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        # Just build one figure.
+        nameOut = 'Figure' + sys.argv[1]
 
-    if parallel is True:
-        pool = multiprocessing.Pool()
-        result = pool.map_async(runFunc, figList)
+        a = runFunc(nameOut)
+
+        if isinstance(a, Exception):
+            raise a
+
     else:
-        list(map(runFunc, figList))
+        if parallel is True:
+            pool = multiprocessing.Pool()
+            result = pool.map_async(runFunc, figList)
+        else:
+            list(map(runFunc, figList))
 
-    # Output data table
-    StoneModelMouse().writeModelData('./Manuscript/Text/07_ModelData.md')
+        # Output data table
+        StoneModelMouse().writeModelData('./Manuscript/Text/07_ModelData.md')
 
-    # IF we're running in parallel we need to wait for Figure 2 to finish
-    # Close the pool
-    if parallel is True:
-        pool.close()
-        pool.join()
+        # IF we're running in parallel we need to wait for Figure 2 to finish
+        # Close the pool
+        if parallel is True:
+            pool.close()
+            pool.join()
 
-        for i in result.get():
-            if isinstance(i, Exception):
-                raise i
+            for i in result.get():
+                if isinstance(i, Exception):
+                    raise i
 
-    # Overlay Figure 2 cartoon
-    template = st.fromfile(fdir + 'Figure2.svg')
-    cartoon = st.fromfile('./recepmod/figures/Figure2_model_diagram.svg').getroot()
+        # Overlay Figure 2 cartoon
+        template = st.fromfile(fdir + 'Figure2.svg')
+        cartoon = st.fromfile('./recepmod/figures/Figure2_model_diagram.svg').getroot()
 
-    cartoon.moveto(10, -15)
+        cartoon.moveto(10, -15)
 
-    template.append(cartoon)
-    template.save(fdir + 'Figure2.svg')
+        template.append(cartoon)
+        template.save(fdir + 'Figure2.svg')
 
-    # Overlay Figure 3 cartoon
-    template = st.fromfile(fdir + 'Figure3.svg')
-    cartoon = st.fromfile('./recepmod/figures/Figure3_model_diagram.svg').getroot()
+        # Overlay Figure 3 cartoon
+        template = st.fromfile(fdir + 'Figure3.svg')
+        cartoon = st.fromfile('./recepmod/figures/Figure3_model_diagram.svg').getroot()
 
-    cartoon.moveto(173, 160, scale=0.16)
+        cartoon.moveto(173, 160, scale=0.16)
 
-    template.append(cartoon)
-    template.save(fdir + 'Figure3.svg')
+        template.append(cartoon)
+        template.save(fdir + 'Figure3.svg')
 
-    # Overlay Figure 4 cartoon
-    template = st.fromfile(fdir + 'Figure4.svg')
-    cartoon = st.fromfile('./recepmod/figures/Figure4_regression_approach.svg').getroot()
+        # Overlay Figure 4 cartoon
+        template = st.fromfile(fdir + 'Figure4.svg')
+        cartoon = st.fromfile('./recepmod/figures/Figure4_regression_approach.svg').getroot()
 
-    cartoon.moveto(10, 20, scale=0.66)
+        cartoon.moveto(10, 20, scale=0.66)
 
-    template.append(cartoon)
-    template.save(fdir + 'Figure4.svg')
+        template.append(cartoon)
+        template.save(fdir + 'Figure4.svg')
