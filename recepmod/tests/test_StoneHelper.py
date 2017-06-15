cb4f047348d3452671b904150b0dd09048b7199a
Aaron Meyer <a@ameyer.me>
2017-06-15 12:26:29 -0400

    Regression fix (#149)
    * Linting
    * Fixed Kx calc.
    * Matched NK receptor expression to activating receptors of other cell line.
    * Present error squared instead of absolute value.
    * Think regression is working again
    * Switch to r2
    * Consolidated regression code.
    * Fix annoying warning from pandas.
    * Switch to using r_square, and setup regression with NK cell fix.
    * No longer need to track 2B KO.

--- recepmod/tests/test_StoneHelper.py
+++ recepmod/tests/test_StoneHelper.py
@@ -1,6 +1,9 @@
 import unittest
 import numpy
 from ..StoneModel import StoneModel
+import warnings
+warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
+
 
 class TestStoneMethods(unittest.TestCase):
     def setUp(self):
