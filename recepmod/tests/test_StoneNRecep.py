62fe62dfb7136c64dff7c5bea5800f2a54e512d7
Aaron Meyer <a@ameyer.me>
2017-07-17 15:16:16 -0400

    More linting.

--- recepmod/tests/test_StoneNRecep.py
+++ recepmod/tests/test_StoneNRecep.py
@@ -2,31 +2,32 @@ import unittest
 import numpy as np
 from ..StoneNRecep import StoneVgrid, reqSolver, StoneRbnd, StoneN
 
+
 class TestStoneNRecpMethods(unittest.TestCase):
     def test_StoneVgrid(self):
-        Req = np.array([1000, 1000], dtype = np.float64)
-        Ka = np.array([1000, 2000], dtype = np.float64)
+        Req = np.array([1000, 1000], dtype=np.float64)
+        Ka = np.array([1000, 2000], dtype=np.float64)
         L0 = 1.000E-7
         Kx = 1.0E-5
         gnu = 4
 
-        output = StoneVgrid(Req,Ka,gnu,Kx,L0)
+        output = StoneVgrid(Req, Ka, gnu, Kx, L0)
 
         # Check that the grid is setup appropriately
-        for ii in range(gnu+1):
-            for jj in range(gnu+1):
-                if ii+jj > gnu:
-                    self.assertEqual(output[ii,jj], 0)
+        for ii in range(gnu + 1):
+            for jj in range(gnu + 1):
+                if ii + jj > gnu:
+                    self.assertEqual(output[ii, jj], 0)
                 elif ii == 0 and jj == 0:
-                    self.assertEqual(output[ii,jj], 0)
+                    self.assertEqual(output[ii, jj], 0)
                 else:
-                    self.assertNotEqual(output[ii,jj], 0)
+                    self.assertNotEqual(output[ii, jj], 0)
 
         # Kx is specified in the reference of the first receptor
-        Kx = Ka[1]/Ka[0]*Kx
+        Kx = Ka[1] / Ka[0] * Kx
 
         # Assemble the grid for the mirror case
-        output2 = StoneVgrid(np.flipud(Req),np.flipud(Ka),gnu,Kx,L0)
+        output2 = StoneVgrid(np.flipud(Req), np.flipud(Ka), gnu, Kx, L0)
 
         # Check that detailed balance holds
         self.assertTrue(np.all(np.isclose(output, np.transpose(output2))))
@@ -53,16 +54,15 @@ class TestStoneNRecpMethods(unittest.TestCase):
         for perm in permutations(a):
             self.assertEqual(boundMult(perm), 20, "Testing vector: " + str(perm))
 
-
     def test_vGridVar(self):
         """ Test that varying Req and Ka provides the expected results. """
-        Req = np.array([1E4, 1E3], dtype = np.float64)
-        Ka = np.array([1E3, 1E3], dtype = np.float64)
+        Req = np.array([1E4, 1E3], dtype=np.float64)
+        Ka = np.array([1E3, 1E3], dtype=np.float64)
         L0 = 1.000E-7
         Kx = 1.0E-5
         gnu = 4
 
-        output = StoneVgrid(Req,Ka,gnu,Kx,L0)
+        output = StoneVgrid(Req, Ka, gnu, Kx, L0)
         self.assertGreater(output[3, 0], output[0, 3])
 
         Req = np.flipud(Req)
@@ -76,21 +76,21 @@ class TestStoneNRecpMethods(unittest.TestCase):
         self.assertGreater(output[3, 0], output[0, 3])
 
         Ka = np.flipud(Ka)
-        output = StoneVgrid(Req,Ka,gnu,Kx,L0)
-        self.assertLess(output[3,0], output[0,3])
+        output = StoneVgrid(Req, Ka, gnu, Kx, L0)
+        self.assertLess(output[3, 0], output[0, 3])
 
     def test_StoneRbnd(self):
         from ..StoneNRecep import StoneRmultiAll
 
         gnu = 4
 
-        vGrid = np.ones([gnu+1, gnu+1], dtype = np.float64)
-        vGrid[0,0] = 0
+        vGrid = np.ones([gnu + 1, gnu + 1], dtype=np.float64)
+        vGrid[0, 0] = 0
 
         for ii in range(vGrid.shape[0]):
             for jj in range(vGrid.shape[0]):
-                if ii+jj > gnu:
-                    vGrid[ii,jj] = 0
+                if ii + jj > gnu:
+                    vGrid[ii, jj] = 0
 
         output = StoneRbnd(vGrid)
         outputmulti = StoneRmultiAll(vGrid)
@@ -100,7 +100,7 @@ class TestStoneNRecpMethods(unittest.TestCase):
         self.assertEqual(outputmulti[0], 19)
         self.assertEqual(outputmulti[1], 19)
 
-        vGrid[2,0] = 2
+        vGrid[2, 0] = 2
 
         output = StoneRbnd(vGrid)
         outputmulti = StoneRmultiAll(vGrid)
@@ -119,22 +119,21 @@ class TestStoneNRecpMethods(unittest.TestCase):
         Kx = 1E-5
         L0 = 1E-5
 
-        Rbnd = StoneRbnd(StoneVgrid(np.power(10,logR),Ka,gnu,Kx,L0))
+        Rbnd = StoneRbnd(StoneVgrid(np.power(10, logR), Ka, gnu, Kx, L0))
 
         self.assertGreater(Rbnd[0], Rbnd[1])
 
         logR = np.flipud(logR)
 
-        Rbnd = StoneRbnd(StoneVgrid(np.power(10,logR),Ka,gnu,Kx,L0))
+        Rbnd = StoneRbnd(StoneVgrid(np.power(10, logR), Ka, gnu, Kx, L0))
 
         self.assertGreater(Rbnd[1], Rbnd[0])
-
 
         logR[0:2] = 2.0
 
         Ka[0:2] = [1E4, 1E5]
 
-        Rbnd = StoneRbnd(StoneVgrid(np.power(10,logR),Ka,gnu,Kx,L0))
+        Rbnd = StoneRbnd(StoneVgrid(np.power(10, logR), Ka, gnu, Kx, L0))
 
         self.assertGreater(Rbnd[1], Rbnd[0])
 
@@ -145,15 +144,15 @@ class TestStoneNRecpMethods(unittest.TestCase):
 
         output = reqSolver(logR, Ka, gnu, Kx, L0)
 
-        vGrid = StoneVgrid(np.power(10,output),Ka,gnu,Kx,L0)
+        vGrid = StoneVgrid(np.power(10, output), Ka, gnu, Kx, L0)
 
         Rbnd = StoneRbnd(vGrid)
 
-        Rtot = np.power(10,output) + Rbnd
+        Rtot = np.power(10, output) + Rbnd
 
-        deltaR = np.linalg.norm(Rtot - np.power(10,logR))
+        deltaR = np.linalg.norm(Rtot - np.power(10, logR))
 
-        self.assertAlmostEqual(deltaR, 0.0, delta = 0.1, msg = "Mass balance failed on reqSolver")
+        self.assertAlmostEqual(deltaR, 0.0, delta=0.1, msg="Mass balance failed on reqSolver")
 
         return (output, vGrid, Rbnd)
 
