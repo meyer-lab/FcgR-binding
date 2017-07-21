f1b45c06afa359652306577b864fcda1ff94e0c9
robbobbinett <rrobinet@mit.edu>
2017-07-21 15:21:02 -0400

    multiply by 100 for percent

--- recepmod/StoneModMouse.py
+++ recepmod/StoneModMouse.py
@@ -224,7 +224,7 @@ class StoneModelMouse:
         
         ## Convert percents into strings depicting percents
         def percent(val):
-            strval = str(val)
+            strval = str(val*100.0)
             return r'$$'+strval[::len(strval)-1]+'\%$$'
 
         ## Rename columns of DataFrame 
