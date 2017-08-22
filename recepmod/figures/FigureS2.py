137336481abe9b02a2a26c5a2fe4cb20e7b737d4
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2017-08-22 12:16:19 -0700

    Refocus figures on cMO (#209)
    * Switch focus to cMO
    * Replot
    * Tweak Fig 4 range
    * New robustness.
    * Just calculate robustness directly.
    * No longer using tqdm
    * Add summplement caption
    * Only import at top of file.

--- recepmod/figures/FigureS2.py
+++ recepmod/figures/FigureS2.py
@@ -5,7 +5,9 @@ import os
 import string
 import pandas as pd
 import seaborn as sns
+import numpy as np
 from .FigureCommon import getSetup, Legend, subplotLabel
+from ..StoneModMouseFit import InVivoPredict
 
 # TODO: Fix legend for this figure
 
@@ -59,37 +61,16 @@ def FcgRexpression(ax):
     # TODO: Fix references to supplemental figures here
 
 
-def robustnessCalc(calculate=True):
-    """ Calculate robustness or load it. """
-    filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                            "../data/figS2-robustness.csv")
-
-    if calculate is True:
-        from tqdm import tqdm
-        import numpy as np
-        from ..StoneModMouseFit import InVivoPredict
-        # Setup the range of avidity and ligand concentration we'll look at
-        gnus = np.logspace(1, 3, 3, base=2, dtype=np.int)
-        Los = np.logspace(start=-12, stop=-7, num=30, dtype=np.float)
-
-        pp = pd.DataFrame(np.array(np.meshgrid(gnus, Los)).T.reshape(-1, 2),
-                          columns=['gnus', 'Los'])
-
-        tqdm.pandas(desc="Condition calculations")
-
-        pp['CPredict'] = pp.progress_apply(lambda x: InVivoPredict(x.as_matrix())[1], axis=1)
-
-        pp.to_csv(filepath)
-    else:
-        # Load the data from CSV
-        pp = pd.read_csv(filepath, index_col=0)
-
-    return pp
-
-
-def robustnessPlot(ax, calculate=False):
+def robustnessPlot(ax):
     """ Vary IC concentration and avidity and show the prediction still stands. """
-    pp = robustnessCalc(calculate)
+    # Setup the range of avidity and ligand concentration we'll look at
+    gnus = np.logspace(1, 3, 3, base=2, dtype=np.int)
+    Los = np.logspace(start=-12, stop=-7, num=30, dtype=np.float)
+
+    pp = pd.DataFrame(np.array(np.meshgrid(gnus, Los)).T.reshape(-1, 2),
+                      columns=['gnus', 'Los'])
+
+    pp['CPredict'] = pp.apply(lambda x: InVivoPredict(x.as_matrix())[1], axis=1)
 
     # Change avidities to strings
     pp['gnus'] = pp['gnus'].apply(lambda gnu: r'$\nu=' + str(int(gnu)) + '$')
