e03eaa2d2d45036feeb372ec6405856f7507049d
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2017-08-29 16:08:15 -0700

    Update StoneNRecep.py

--- recepmod/StoneNRecep.py
+++ recepmod/StoneNRecep.py
@@ -189,11 +189,9 @@ class StoneN:
         """ Return the activity index. """
         summ = np.dot(self.getRmultiAll(), actV)
 
-        if summ >= 0:
-            return summ
-        elif summ < 0:
+        if summ < 0:
             return 0.0
-        return 0.0
+        return summ
 
     def __init__(self, logR, Ka, Kx, gnu, L0):
         self.logR = np.array(logR, dtype=np.float, copy=True)
